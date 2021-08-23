8ec873f3b2739081ff3827f2e1ab7e6c414544c5
Paul <gafgafgarion05@gmail.com>
2021-08-23 17:34:23 +0300

    added def

--- delete_jpg_from_desktop.py
+++ delete_jpg_from_desktop.py
@@ -1,13 +1,17 @@
 import os
-import pathlib
 
 path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-
-
 files_in_directory = os.listdir(path)
-filtered_files = [file for file in files_in_directory if file.endswith(".txt")]
 
-for file in filtered_files:
-	path_to_file = os.path.join(path, file)
-	os.remove(path_to_file)
 
+def delete_files(type_files='.txt'):
+    filtered_files = [file for file in files_in_directory if file.endswith(type_files)]
+
+    for file in filtered_files:
+        path_to_file = os.path.join(path, file)
+        os.remove(path_to_file)
+
+
+delete_files()
+delete_files('jpg')
+delete_files('png')
