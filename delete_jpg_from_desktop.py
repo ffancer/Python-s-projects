d8f87efa7da43bc689b2a7872487bde5e849dd96
Paul <gafgafgarion05@gmail.com>
2021-08-23 15:19:37 +0300

    can delete 1 file

--- delete_jpg_from_desktop.py
+++ delete_jpg_from_desktop.py
@@ -1,7 +1,9 @@
 import os
+import pathlib
 
 path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
 file = 'test2.txt'
 
-with open(os.path.join(path, file), 'w') as fp:
-    pass
+file = pathlib.Path(path + "/test.txt")
+file.unlink()
+
