4d7d4bafdde65732d0b0f3f8de143e57b7fb234e
Paul <gafgafgarion05@gmail.com>
2021-08-23 16:02:39 +0300

    can delete 1 type of files (.txt .png etc.)

--- delete_jpg_from_desktop.py
+++ delete_jpg_from_desktop.py
@@ -2,8 +2,12 @@ import os
 import pathlib
 
 path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-file = 'test2.txt'
 
-file = pathlib.Path(path + "/test.txt")
-file.unlink()
+
+files_in_directory = os.listdir(path)
+filtered_files = [file for file in files_in_directory if file.endswith(".txt")]
+
+for file in filtered_files:
+	path_to_file = os.path.join(path, file)
+	os.remove(path_to_file)
 
