de96169e8baed57364f0d42058f54f72e64af0e7
Paul <gafgafgarion05@gmail.com>
2021-08-23 14:58:10 +0300

    come to desktop path and create new txt file

--- delete_jpg_from_desktop.py
+++ delete_jpg_from_desktop.py
@@ -1,2 +1,7 @@
-# test
-print(1 + 5)
+import os
+
+path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+file = 'test2.txt'
+
+with open(os.path.join(path, file), 'w') as fp:
+    pass
