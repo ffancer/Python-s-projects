970db7d5825dc1b427837c522b76a8d32c073388
Paul <gafgafgarion05@gmail.com>
2021-11-26 10:30:23 +0300

    solution

--- Find the number of distinct elements of an array.py
+++ Find the number of distinct elements of an array.py
@@ -2,5 +2,9 @@
 # Find the number of distinct elements in the array. Example: for 1 4 5 1 1 3 the answer is 4.
 
 
-lst = [1,4,5,1,1,3]
-print(len(set(lst)))
+def number_of_distinct(lst):
+    return f"number of distinct elements in the {lst} - {len(set(lst))}"
+
+
+print(number_of_distinct([1, 4, 5, 1, 1, 3]))
+
