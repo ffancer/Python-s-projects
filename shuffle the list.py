cbe6859b1633a05abcf14369a4ed3960a20dbaa5
Paul <gafgafgarion05@gmail.com>
2021-11-26 10:41:53 +0300

    init

---
+++ shuffle the list.py
@@ -0,0 +1,3 @@
+# Given an array. Shuffle its elements randomly so that each element is in a different place.
+# Дан массив. Перемешать его элементы случайным образом так, чтобы каждый элемент оказался на новом месте.
+
