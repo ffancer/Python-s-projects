b8b827ba59f669fbec910b6b20b6a01752d3e68a
Paul <gafgafgarion05@gmail.com>
2021-11-26 10:52:25 +0300

    testing

--- shuffle the list.py
+++ shuffle the list.py
@@ -1,3 +1,16 @@
 # Given an array. Shuffle its elements randomly so that each element is in a different place.
 # Дан массив. Перемешать его элементы случайным образом так, чтобы каждый элемент оказался на новом месте.
 
+from random import shuffle
+
+def shuffle_list(lst):
+    arr = tuple(lst)
+    shuffle(lst)
+    return lst, arr
+
+
+print(shuffle_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+print(shuffle_list(['a', 'b', 'c', 'd']))
+print(shuffle_list(['one', '777', 12, 54, [1, 2]]))
+
+
