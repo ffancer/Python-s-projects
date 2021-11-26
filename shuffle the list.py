ac9db382178775277acae9f50aa8667497336f0e
Paul <gafgafgarion05@gmail.com>
2021-11-26 13:29:18 +0300

    second decision

--- shuffle the list.py
+++ shuffle the list.py
@@ -1,26 +1,22 @@
 # Given an array. Shuffle its elements randomly so that each element is in a different place.
 # Дан массив. Перемешать его элементы случайным образом так, чтобы каждый элемент оказался на новом месте.
 
-from random import shuffle
-from copy import copy
+from random import randint
 
-
-# def shuffle_list(lst):
-#     old_lst = copy(lst)
-#     shuffle(lst)
-#     new_lst = lst
-#
-#     if old_lst != new_lst:
-#         return lst
-#     shuffle(new_lst)
-#     # return new_lst
 
 def shuffle_list(lst):
-    return lst[-1:]+lst[:-1]
+    new_list = lst[:]
+    list_len = len(new_list)
+
+    for i in range(list_len):
+        rdm_index = randint(0, list_len - 1)
+        temp = new_list[i]
+        new_list[i] = new_list[rdm_index]
+        new_list[rdm_index] = temp
+
+    return new_list
 
 
 print(shuffle_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
 print(shuffle_list(['a', 'b', 'c', 'd']))
-print(shuffle_list(['one', '777', 12, 54, [1, 2]]))
-
-# print([1,2,3] == [1,2])
+print(shuffle_list([11, '1111', [1, 2], 'b']))
