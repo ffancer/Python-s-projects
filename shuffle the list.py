4e9555e8702f42e961381301040ea312f62a5d48
Paul <gafgafgarion05@gmail.com>
2021-11-26 13:31:26 +0300

    final decision, first version if old_list == new_list will be None ;)

--- shuffle the list.py
+++ shuffle the list.py
@@ -1,20 +1,15 @@
 # Given an array. Shuffle its elements randomly so that each element is in a different place.
 # Дан массив. Перемешать его элементы случайным образом так, чтобы каждый элемент оказался на новом месте.
 
-from random import randint
+from random import shuffle
 
 
 def shuffle_list(lst):
-    new_list = lst[:]
-    list_len = len(new_list)
+    old_lst = lst[:]
+    shuffle(lst)
 
-    for i in range(list_len):
-        rdm_index = randint(0, list_len - 1)
-        temp = new_list[i]
-        new_list[i] = new_list[rdm_index]
-        new_list[rdm_index] = temp
-
-    return new_list
+    while lst != old_lst:
+        return lst
 
 
 print(shuffle_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
