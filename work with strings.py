b1e4ab255b2f407c2b8d7ea5156e7ffb9217e879
Paul <gafgafgarion05@gmail.com>
2021-11-29 11:01:47 +0300

    my solution of this problem, but I want 1 big letter in sentence :)

--- work with strings.py
+++ work with strings.py
@@ -1,6 +1,13 @@
 # Дано предложение. Заменить группы пробелов одиночными, крайние пробелы удалить.
 # Все слова перевести в нижний регистр, первые буквы сделать заглавными.
+# A sentence is given. Replace groups of spaces with single ones, remove trailing spaces.
+# Convert all words to lower case, capitalize the first letters.
 
-s = '    ВечерелО     мужчины        шли   с РАБОТы     '
-s = s.split()
-print(' '.join(s).lower().title())
+def work_with_sentence(s):
+    s = s.split()
+    return' '.join(s).lower().title()
+
+
+print(work_with_sentence('   BOB  wAnNa     be   a  doctor   '))
+print(work_with_sentence('1    2   3  4  5         6'))
+print(work_with_sentence('  margo     has     22     eggs             '))
