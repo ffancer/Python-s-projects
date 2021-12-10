37206073e3dbdc842dcedca4d0c59570812a44ef
Paul <gafgafgarion05@gmail.com>
2021-12-10 10:35:46 +0300

    ru version

--- random wikipedia article.py
+++ random wikipedia article.py
@@ -1,6 +1,23 @@
 import requests
+from bs4 import BeautifulSoup
+import webbrowser
 
-response = requests.get(
-	url="https://en.wikipedia.org/wiki/Web_scraping",
-)
-print(response.status_code)
+while True:
+    url = requests.get("https://ru.wikipedia.org/wiki/Special:Random")
+    soup = BeautifulSoup(url.content, "html.parser")
+    title = soup.find(class_="firstHeading").text
+
+    print(f"{title} \nDo you want to view it? (Y/N)")
+    answer = input("").lower()
+
+    if answer == "y":
+        url = "https://ru.wikipedia.org/wiki/%s" % title
+        webbrowser.open(url)
+        break
+    elif answer == "n":
+        print("Try again!")
+        continue
+    else:
+        print("Wrong choice!")
+        break
+
