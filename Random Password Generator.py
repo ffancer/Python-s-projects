8f04e5672c87d6041e1139f62b540f97f606b48b
Paul <gafgafgarion05@gmail.com>
2021-12-14 10:43:01 +0300

    Added the ability to exclude symbols (^%$& etc.) but spaghetti code, need do it more readable, I think need add definition

--- Random Password Generator.py
+++ Random Password Generator.py
@@ -2,17 +2,29 @@ from string import ascii_letters, digits, punctuation
 from random import sample
 
 print('Welcome to Password generator version 1.0\nFor quit enter "q"')
-symbols = ascii_letters + digits + punctuation
+symbols = ascii_letters + digits
 
 while True:
+    choice = input('Do you want symbols (^&$#@ etc.) in your password? Y/N: ').lower()
     length = input('Enter the length of password: ')
-    if length == 'q':
-        print('Come back :)')
-        break
-    elif length.isdigit():
-        print(''.join(sample(symbols, int(length))))
-    else:
-        continue
+    if choice == 'y':
+        symbols += punctuation
+        if length == 'q':
+            print('Come back :)')
+            break
+        elif length.isdigit():
+            print(''.join(sample(symbols, int(length))))
+        else:
+            continue
+    elif choice == 'n':
+        if length == 'q':
+            print('Come back :)')
+            break
+        elif length.isdigit():
+            print(''.join(sample(symbols, int(length))))
+        else:
+            continue
+
 
 
 
