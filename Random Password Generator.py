168c9156fa5fa7b9035b85d0f17e1ce4a7a69f7d
Paul <gafgafgarion05@gmail.com>
2021-12-14 09:59:42 +0300

    All is working. I thought about random.choice and random.sample. Choose sample because it has length parameter. Add 5 tests.

--- Random Password Generator.py
+++ Random Password Generator.py
@@ -1,10 +1,17 @@
 import string
+import random
 
 
 def pass_generator(length):
     symbols = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.sample(symbols, length))
 
 
+print(pass_generator(20))
+print(pass_generator(12))
+print(pass_generator(10))
+print(pass_generator(8))
+print(pass_generator(4))
 
 
 
