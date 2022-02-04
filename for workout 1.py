1d4e005b0834709d48b3524d837ff440acd95e8c
Paul <gafgafgarion05@gmail.com>
2022-02-04 10:55:03 +0300

    1 "skeleton" is ready

--- for workout 1.py
+++ for workout 1.py
@@ -1,6 +1,16 @@
 """
 Сегодня я решил тренироваться используя метод "лесенка". Для примера: 1 подтягивание - 2 подтягивания - 3 подтягивания - 2 - 1.
-Задача: подсчитать общее колличество повторения за всю лесенку
+Задача: подсчитать общее колличество повторения за всю лесенку.
+///
+Today I decided to train using the "ladder" method. For example: 1 pull-up - 2 pull-ups - 3 pull-ups - 2 - 1.
+Task: Count the total number of repetitions for the entire "ladder".
 """
 
 
+n = 1
+total = 0
+for i in range(n, n*5+1, n):
+    total += i
+for j in range(n*4,n-1, -n):
+    total += j
+print(total)
