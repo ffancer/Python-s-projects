cf48228c7446df85d144bdd6f3c4cbc4e0152e50
Paul <gafgafgarion05@gmail.com>
2022-02-21 10:05:25 +0300

    make our board

--- tic tac toe.py
+++ tic tac toe.py
@@ -3,3 +3,10 @@ Our algorithm:
 1)board 2)display board 3)play game 4)handle turn 5)check win (check rows/columns/diagonals)
 6)check tie 7)flip player
 """
+
+board = ['-', '-', '-',
+         '-', '-', '-',
+         '-', '-', '-']
+
+
+
