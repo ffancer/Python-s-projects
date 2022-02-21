b34c94f60025d6f58f88658a99e8e6d9e5720f14
Paul <gafgafgarion05@gmail.com>
2022-02-21 13:19:20 +0300

    make 'rows and win': if 0,1,2 or 3,4,5 or 6,7,8 are X = X win and out from game

--- tic tac toe.py
+++ tic tac toe.py
@@ -70,7 +70,18 @@ def check_for_winner():
         winner = None
     return
 def check_rows():
-    return
+    global game_still_going
+    row_1 = board[0] == board[1] == board[2] != '-'
+    row_2 = board[3] == board[4] == board[5] != '-'
+    row_3 = board[6] == board[7] == board[8] != '-'
+    if row_1 or row_2 or row_3:
+        game_still_going = False
+    if row_1:
+        return board[0]
+    elif row_2:
+        return board[3]
+    elif row_3:
+        return board[6]
 def check_columns():
     return
 def check_diagonals():
