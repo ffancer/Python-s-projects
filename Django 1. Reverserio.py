7447c64c242114866b045c8a3ebb5d0a81c6d749
Paul <gafgafgarion05@gmail.com>
2022-07-13 10:41:55 +0300

    primitive work with classes methods 1

--- Django 1. Reverserio.py
+++ Django 1. Reverserio.py
@@ -8,11 +8,9 @@ class Car:
         self.year = year
         self.is_crashed = is_crashed
 
+    def drive(self):
+        print(f'{self.name.title()} is driving.')
 
-bmw_car = Car(name='BMW x5', color='Blue')
-mazda_car = Car(name='mazda X67', year=1999, is_crashed=False)
-
-print(bmw_car.year, mazda_car.year, bmw_car.is_crashed, mazda_car.wheels_number, sep='\n')
-
-how_many_wheels_have_5_cars = Car.wheels_number * 5
-print(f'5 cars have {how_many_wheels_have_5_cars} wheels')
+opel_car = Car('opel Astra', 'red', 2016, False)
+opel_car.drive()
+print(opel_car.color)
