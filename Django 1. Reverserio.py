da2319d28464e44a1c322d8a21c296e1d4bd25f7
Paul <gafgafgarion05@gmail.com>
2022-07-13 09:14:33 +0300

    primitive work with classes 2

--- Django 1. Reverserio.py
+++ Django 1. Reverserio.py
@@ -1,9 +1,11 @@
 class Car:
-    def __init__(self, name):
+    def __init__(self, name='_', color='_', year='_'):
         self.name = name
+        self.color = color
+        self.year = year
 
 
-bmw = Car(name='BMW x5')
+bmw_car = Car(name='BMW x5', color='Blue')
+mazda_car = Car(name='mazda X67', year='1999')
 
-
-print(bmw.name)
+print(bmw_car.year, mazda_car.year, sep='\n')
