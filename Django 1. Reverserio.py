bd5ebf95369745d93d8168147fd7c711d1344dc4
Paul <gafgafgarion05@gmail.com>
2022-07-13 10:51:38 +0300

    primitive work with classes methods 2

--- Django 1. Reverserio.py
+++ Django 1. Reverserio.py
@@ -1,5 +1,4 @@
 class Car:
-
     wheels_number = 4
 
     def __init__(self, name='_', color='_', year=2020, is_crashed=True):
@@ -8,9 +7,14 @@ class Car:
         self.year = year
         self.is_crashed = is_crashed
 
-    def drive(self):
-        print(f'{self.name.title()} is driving.')
+    def drive(self, city):
+        print(f'{self.name.title()} is driving to {city}.')
+
+    def change_color(self, new_color):
+        self.color = new_color
+
 
 opel_car = Car('opel Astra', 'red', 2016, False)
-opel_car.drive()
+opel_car.drive('Moscow')
+opel_car.change_color('purple')
 print(opel_car.color)
