4531199a21da251b8d4028320a1011967a88f487
Paul <gafgafgarion05@gmail.com>
2022-08-10 13:57:15 +0300

    -

--- parser_goodgame.py
+++ parser_goodgame.py
@@ -26,17 +26,23 @@ soup = BeautifulSoup(src, 'lxml')
 
 # all_topics_info = soup.find_all(class_='thread-block')
 # all_topics_info = soup.find('div', class_='thread-block')
-all_topics_info = soup.find(class_='thread-block')
-topics_name = all_topics_info.find_all(class_='name')
-topics_count = all_topics_info.find_all(class_='count')
-topics_href = all_topics_info.find_all('a', class_='creater')
+all_topics_info = soup.find_all('div', class_='theme')
+# print(all_topics_info)
 
-# for i, j in enumerate(topics_name, 1):
-#     print(i, j.text)
+for item in all_topics_info:
 
-# for item in topics_count:
-#     print(item.text)
+    topics_name = item.find_all(class_='name')
+    topics_count = item.find_all(class_='count')
+    topics_href = item.find_all('a', class_='creater')
 
-for item in topics_href:
-    print(item.get('href'))
+    print(topics_href)
+
+    # for i, j in enumerate(topics_name, 1):
+    #     print(i, j.text)
+
+    # for item in topics_count:
+    #     print(item.text)
+
+    # for item in topics_href:
+    #     print(item.get('href'))
 
