093979602a9b22226525fea626641af142be87bd
Paul <gafgafgarion05@gmail.com>
2022-08-11 15:44:15 +0300

    took -topics -counts -links

--- parser_goodgame.py
+++ parser_goodgame.py
@@ -22,25 +22,25 @@ with open('index.html', encoding="utf-8-sig") as file:
 
 soup = BeautifulSoup(src, 'lxml')
 
-
+# print(type(soup))
 # all_topics_info = soup.find_all('div', class_='theme')
 all_topics_info = soup.find_all('div', class_='forum-block')
-print(all_topics_info)
 
-# for item in all_topics_info:
-#
-#     topics_name = item.find_all(class_='name')
-#     topics_count = item.find_all(class_='count')
-#     topics_href = item.find_all('a', class_='creater')
-#
-#     print(topics_href)
 
-    # for i, j in enumerate(topics_name, 1):
-    #     print(i, j.text)
+for item in all_topics_info:
 
-    # for item in topics_count:
-    #     print(item.text)
+    topics_name = item.find_all(class_='name')
+    topics_count = item.find_all(class_='count')
+    topics_href = item.find_all('a', class_='creater')
 
-    # for item in topics_href:
-    #     print(item.get('href'))
+    for i in topics_name:
+        print(i.text.strip())
+
+    for i in topics_count:
+        print(i.text)
+
+    for i in topics_href:
+        print(i.get('href'))
+
+
 
