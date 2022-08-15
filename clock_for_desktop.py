8db40bac403c1e5189486b61d43b968aaa69d1ab
Paul <gafgafgarion05@gmail.com>
2022-08-15 15:26:53 +0300

    init

---
+++ clock_for_desktop.py
@@ -0,0 +1,6 @@
+"""
+I needed a clock that would show the time at the top of all the windows. The watch should be in the middle of the top.
+If you played Grim Dawn you know about what I want
+
+Мне нужны часы, которые будут висеть поверх всех окон, в центре и наверху. Как в игре Grim Dawn, если вы в курсе :)
+"""
