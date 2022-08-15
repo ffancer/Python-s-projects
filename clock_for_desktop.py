8a425e807d914eb664a8c6b31bffb1a4436a268b
Paul <gafgafgarion05@gmail.com>
2022-08-15 15:33:02 +0300

    -

--- clock_for_desktop.py
+++ clock_for_desktop.py
@@ -4,3 +4,27 @@ If you played Grim Dawn you know about what I want
 
 Мне нужны часы, которые будут висеть поверх всех окон, в центре и наверху. Как в игре Grim Dawn, если вы в курсе :)
 """
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+
+root = Tk()
+root.eval('tk::PlaceWindow . center')
+root.title('Clock')
+
+
+def time():
+    string = strftime('%H:%M:%S')
+    lbl.config(text=string)
+    lbl.after(1000, time)
+
+
+lbl = Label(root, font=('calibri', 40, 'bold'),
+            background='purple',
+            foreground='white')
+
+lbl.pack(anchor='n')
+time()
+
+mainloop()
