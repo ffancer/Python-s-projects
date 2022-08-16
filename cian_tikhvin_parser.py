5fd63621783ced449493bb2db15ac3eb3b5a8729
Paul <gafgafgarion05@gmail.com>
2022-08-16 12:30:56 +0300

    try to save index.html but was error

--- cian_tikhvin_parser.py
+++ cian_tikhvin_parser.py
@@ -3,4 +3,18 @@
 Searching 1-2 room apartments in Tikhvin, Len. region, for the purpose of purchase.
 """
 
+import requests
+from bs4 import BeautifulSoup
 
+url = 'https://spb.cian.ru/kupit-kvartiru-1-komn-ili-2-komn-leningradskaya-oblast-tihvin'
+
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+}
+
+req = requests.get(url, headers=headers)
+src = req.text
+
+with open('index.html', 'w') as file:
+    file.write(src)
