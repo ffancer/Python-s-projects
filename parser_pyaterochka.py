b12f2b5815c032c23fedfc1004dc664ab1d26a57
Paul <gafgafgarion05@gmail.com>
2022-08-29 12:40:20 +0300

    default request work fine, no block

--- parser_pyaterochka.py
+++ parser_pyaterochka.py
@@ -1,3 +1,20 @@
 """
 Будем парсить пятерочку
 """
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://5ka.ru/special_offers/'
+
+# headers = {
+#     'Accept': '*/*',
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+# }
+headers = {
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+}
+req = requests.get(url, headers=headers)
+src = req.text
+
+print(src)
