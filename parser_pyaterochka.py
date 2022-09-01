f15cf8503f0eb306f8fa53187ccfdca36c572aae
Paul <gafgafgarion05@gmail.com>
2022-09-01 13:43:17 +0300

    -

--- parser_pyaterochka.py
+++ parser_pyaterochka.py
@@ -5,7 +5,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 # https://5ka.ru/special_offers/16290
 # https://5ka.ru/?city_id=12270   town id
 # url = 'https://5ka.ru/special_offers/'
@@ -26,10 +25,10 @@ from bs4 import BeautifulSoup
 # print(a)
 
 import time
+from selenium.webdriver import ActionChains
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import chromedriver_binary
-
 
 browser = webdriver.Chrome()
 browser.maximize_window()
@@ -47,18 +46,36 @@ def location_confirm():
 def location_shop():
     search_button_1 = browser.find_element(By.CSS_SELECTOR, '[data-v-663b5104]')
     search_button_1.click()
-    time.sleep(3)
+    time.sleep(1)
     search_button_2 = browser.find_element(By.CSS_SELECTOR, '[data-v-7475baa1]')
     search_button_2.click()
-    search_button_3 = browser.find_element(By.CSS_SELECTOR, '[store - bottom]')
-    search_button_3.click()
     time.sleep(1)
-    # store - bottom
+    # search_button_3 = browser.find_element(By.XPATH,
+    #                                        '//*[@id="__layout"]/main/div[1]/main/div/div/div[1]/section/div[1]/div/div/ul/li[2]/label/span')
+    #
+    # search_button_3.click()
+    search_button_3 = browser.find_element(By.CLASS_NAME, '[class="address"]')
+
+    search_button_3.click()
+    # ActionChains(browser).context_click(search_button_2).perform()
+    # time.sleep(3)
+    # search_button_3 = browser.find_element(By.XPATH,
+    #                                        '//*[@id="__layout"]/main/div[1]/main/div/div/div[1]/section/div[1]/div/div/ul/li[2]/label/span')
+    #
+    # search_button_3.click()
+
+
+# def click():
+#     # search_button_3 = browser.find_element(By.CSS_SELECTOR, '[store - bottom]')
+#     # search_button_3.click()
+#     # time.sleep(1)
+#     search_button_3 = browser.find_element(By.XPATH, '//*[@id="__layout"]/main/div[1]/main/div/div/div[1]/section/div[1]/div/div/ul/li[2]/label/span/p')
+#     search_button_3.click()
 
 
 location_confirm()
 location_shop()
+click()
 
 time.sleep(500)
 browser.close()
-
