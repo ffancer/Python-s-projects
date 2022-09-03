1ad2fa952c06cd4aa9151caa96f8441b8483c352
Paul <gafgafgarion05@gmail.com>
2022-09-03 11:28:53 +0300

    save and open offline web-page for parsing

--- parser_dixy.py
+++ parser_dixy.py
@@ -1,27 +1,29 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import chromedriver_binary
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# import chromedriver_binary
+#
+# browser = webdriver.Chrome()
+# browser.maximize_window()
+# browser.get('https://dixy.ru/catalog/')
+# time.sleep(2)
+#
+# def select_town():
+#     search_button = browser.find_element(By.CLASS_NAME, 'current-city')
+#     search_button.click()
+#     time.sleep(1)
+#     search_button.click()
+#     time.sleep(3)
+#     search_button2 = browser.find_element(By.XPATH, '/html/body/header/div/div/ul[1]/li[1]/div/div[3]/div/ul/li[4]/a')
+#     search_button2.click()
+#
+#
+# select_town()
+# time.sleep(500)
+# browser.close()
 
-browser = webdriver.Chrome()
-browser.maximize_window()
-# browser.get('https://dixy.ru/')
-browser.get('https://dixy.ru/catalog/')
-time.sleep(2)
 
-def select_town():
-    search_button = browser.find_element(By.CLASS_NAME, 'current-city')
-    search_button.click()
-    time.sleep(1)
-    search_button.click()
-    time.sleep(3)
-    search_button2 = browser.find_element(By.XPATH, '/html/body/header/div/div/ul[1]/li[1]/div/div[3]/div/ul/li[4]/a')
-    search_button2.click()
+with open('dixy.html', encoding='utf-8') as file:
+    src = file.read()
 
-
-select_town()
-
-time.sleep(500)
-
-
-browser.close()
+print(src)
