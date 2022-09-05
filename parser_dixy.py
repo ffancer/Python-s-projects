6c5f9d55b7aa394d239e81afb26e3e855baec148
Paul <gafgafgarion05@gmail.com>
2022-09-05 14:03:42 +0300

    -

--- parser_dixy.py
+++ parser_dixy.py
@@ -13,24 +13,35 @@ time.sleep(2)
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
 }
-list_with_names_prices = [[], [], []]
+list_with_names_prices = [[], []]
 
 
 # выбираем ленинградскую область, город не важен (как я понял)
 def select_location():
-    search_button = browser.find_element(By.CLASS_NAME, 'current-city')
-    search_button.click()
+    button_city = browser.find_element(By.CLASS_NAME, 'current-city')
+    button_city.click()
     time.sleep(1)
-    search_button.click()
+    button_city.click()
     time.sleep(3)
-    search_button2 = browser.find_element(By.XPATH, '/html/body/header/div/div/ul[1]/li[1]/div/div[3]/div/ul/li[4]/a')
-    search_button2.click()
+    button_location = browser.find_element(By.XPATH, '/html/body/header/div/div/ul[1]/li[1]/div/div[3]/div/ul/li[4]/a')
+    button_location.click()
 
 
 # достаем 10 листов с карточками товаров в цикле
 def loop_collect_products():
     flag = 9
     while flag != 0:
+        req = requests.get(url, verify=False)
+        soup = BeautifulSoup(req, 'lxml')
+        articles = soup.find_all('div', {'class': 'dixyCatalogItem'})
+
+        for article in articles:
+            name = article.find('div', class_='dixyCatalogItem__title').text.strip()
+            list_with_names_prices[0].append(name)
+            price = article.find('p').text.strip()
+            price_coins = article.find('div', class_='dixyCatalogItemPrice__kopeck').text.strip()
+            list_with_names_prices[1].append(float(price + '.' + price_coins))
+
         search_continue_button = browser.find_element(By.XPATH, '/html/body/section[3]/div/a')
         time.sleep(1)
         search_continue_button.click()
@@ -40,56 +51,21 @@ def loop_collect_products():
 
 # with open('dixy.html', encoding='utf-8') as file:
 #     src = file.read()
-def list_filling():
-# src = requests.get(url, headers=headers)
-    req = requests.get(url, verify=False)
-    soup = BeautifulSoup(req, 'lxml')
-    articles = soup.find_all('div', {'class': 'dixyCatalogItem'})
-    for article in articles:
-        price = article.find('p').text.strip()
-        price_coins = article.find('div', class_='dixyCatalogItemPrice__kopeck').text.strip()
-        name = article.find('div', class_='dixyCatalogItem__title').text.strip()
 
+# src = requests.get(url, headers=headers)
+#     req = requests.get(url, verify=False)
+#     soup = BeautifulSoup(req, 'lxml')
+#     articles = soup.find_all('div', {'class': 'dixyCatalogItem'})
+#     for article in articles:
+#         price = article.find('p').text.strip()
+#         price_coins = article.find('div', class_='dixyCatalogItemPrice__kopeck').text.strip()
+#         name = article.find('div', class_='dixyCatalogItem__title').text.strip()
 
 
 # вызов функций и обработка инфы в течении 500 секунд (нужна корректировка времени), закрытие браузера
 select_location()
+time.sleep(2)
 loop_collect_products()
+print(list_with_names_prices)
 time.sleep(500)
 browser.close()
-
-
-
-
-
-# отладка части кода, были проблемы с респонс и кодами допуска
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://dixy.ru/catalog/'
-headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
-    }
-
-
-lst = []
-req = requests.get(url, verify=False)
-# print(req.text)
-
-soup = BeautifulSoup(req.text, 'html.parser')
-# print(soup.text)
-articles = soup.find_all('div', {'class': 'dixyCatalogItem'})
-for article in articles:
-    price = article.find('p').text.strip()
-    print(price)
-    lst.append(price)
-    # price_coins = article.find('div', class_='dixyCatalogItemPrice__kopeck').text.strip()
-    # name = article.find('div', class_='dixyCatalogItem__title').text.strip()
-#
-print(lst)
-
-
-
-
-
-
