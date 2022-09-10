2ceea5bd9af8ae6b138900a56bc25e7f36c425d4
Paul <gafgafgarion05@gmail.com>
2022-09-10 08:27:48 +0300

    open main page, all correct

--- magnit_parsing.py
+++ magnit_parsing.py
@@ -1,1 +1,11 @@
-url = 'https://magnit.ru/promo/'
+from selenium import webdriver
+import time
+
+url = 'https://magnit.ru/'
+browser = webdriver.Chrome()
+browser.maximize_window()
+browser.get(url)
+
+
+time.sleep(6000)
+browser.close()
