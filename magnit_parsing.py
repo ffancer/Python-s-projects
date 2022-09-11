38fa164d0aec915133267b4f098d0bf48a29c18d
Paul <gafgafgarion05@gmail.com>
2022-09-11 08:45:35 +0300

    select_location is working next steps - scrolling by mouse in the end of page, take all cards with products

--- magnit_parsing.py
+++ magnit_parsing.py
@@ -2,29 +2,36 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 
+
 # url = 'https://magnit.ru/'
 url = 'https://magnit.ru/promo/'
 browser = webdriver.Chrome()
 browser.maximize_window()
 browser.get(url)
-time.sleep(3)
+time.sleep(1)
+
+
 def select_location():
     # закрываем поп-ап
-    browser.find_element(By.XPATH, '/html/body/div[6]/div/a').click()
+    browser.find_element(By.XPATH, '/html/body/div[7]/div/a').click()
+    time.sleep(1)
     # кликаем на "г. Москва"
     browser.find_element(By.XPATH, '/html/body/div[1]/header/div/div[1]/a[1]/span').click()
     time.sleep(2)
     # тут новое для меня - удаление текста и прописка своего
-    button_town = browser.find_element(By.XPATH, '/html/body/div[5]/div/div[2]/div/div[1]/input')
+    button_town = browser.find_element(By.XPATH, '/html/body/div[6]/div/div[2]/div/div[1]/input')
     button_town.clear()
     time.sleep(1)
-    button_town.send_keys('Пикалево')
+    button_town.send_keys('г. Пикалево')
     time.sleep(1)
+    # выбираем город
     browser.find_element(By.CLASS_NAME, 'city-search__link').click()
+    time.sleep(3)
+    # закрываем поп-ап, если появится еще - сделаем переменную
+    browser.find_element(By.XPATH, '/html/body/div[7]/div/a').click()
 
-browser.find_element(By.XPATH, '/html/body/section[3]/div/header/div/div[3]/nav/div[1]/a[1]').click()
 
-
+# город выбрали, теперь осталось скроллить (кнопки "далее" или аналога нет), т.е. скроллим путём перетаскивания мышки
 select_location()
 time.sleep(6000)
 browser.close()
