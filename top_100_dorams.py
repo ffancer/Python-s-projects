3530535c5049b58fd75f06211d0a73108950521f
Paul <gafgafgarion05@gmail.com>
2022-09-14 10:05:59 +0300

    made soup (all works fine), need save webpage and work without web-connect (don't want ban from site)

--- top_100_dorams.py
+++ top_100_dorams.py
@@ -1,3 +1,16 @@
 """
 top 100 dorams from https://vsedoramy.net/top100korean.html
 """
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://vsedoramy.net/top100korean.html'
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+}
+req = requests.get(url, headers=headers)
+soup = BeautifulSoup(req.text, 'lxml')
+
+print(soup)
+
