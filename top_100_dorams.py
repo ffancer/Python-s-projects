16c166265c31179268ce12f8b50b176b268c498c
Paul <gafgafgarion05@gmail.com>
2022-09-14 10:23:08 +0300

    save and open web-page offline

--- top_100_dorams.py
+++ top_100_dorams.py
@@ -9,8 +9,18 @@ headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
 }
-req = requests.get(url, headers=headers)
-soup = BeautifulSoup(req.text, 'lxml')
+# req = requests.get(url, headers=headers)
+# soup = BeautifulSoup(req.text, 'lxml')
 
+# with open('topdorams.html', 'w', encoding='utf-8') as file:
+#     file.write(req.text)
+
+
+with open('topdorams.html', encoding='utf-8') as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, 'lxml')
 print(soup)
+
+
 
