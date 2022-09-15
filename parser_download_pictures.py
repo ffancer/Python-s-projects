17a13295c641b036ed310e50ade1d0fb7bb40da1
Paul <gafgafgarion05@gmail.com>
2022-09-15 08:27:16 +0300

    req + bs4 work ok, will work with webdriver

--- parser_download_pictures.py
+++ parser_download_pictures.py
@@ -2,3 +2,16 @@
 We visit website and download pictures in folder. Let's try
 https://stock.adobe.com/ru/free
 """
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://stock.adobe.com/ru/free'
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+}
+req = requests.get(url, headers=headers)
+soup = BeautifulSoup(req.text, 'lxml')
+
+print(soup)
