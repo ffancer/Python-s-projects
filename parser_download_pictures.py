95f57e78874499433a4baf59cf520279bc4e9427
Paul <gafgafgarion05@gmail.com>
2022-09-15 08:31:12 +0300

    open site, no problems + fast, now we can do def for topic selection

--- parser_download_pictures.py
+++ parser_download_pictures.py
@@ -4,14 +4,22 @@ https://stock.adobe.com/ru/free
 """
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 
 url = 'https://stock.adobe.com/ru/free'
-headers = {
-    'Accept': '*/*',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-}
-req = requests.get(url, headers=headers)
-soup = BeautifulSoup(req.text, 'lxml')
+browser = webdriver.Chrome()
+browser.maximize_window()
+browser.get(url)
+# headers = {
+#     'Accept': '*/*',
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+# }
+# req = requests.get(url, headers=headers)
+# soup = BeautifulSoup(req.text, 'lxml')
 
-print(soup)
+time.sleep(500)
+browser.close()
+
