523fa9aac7a717a01384b7f99e30650acd469827
Paul <gafgafgarion05@gmail.com>
2022-09-16 16:34:32 +0300

    wanna do scrolling to the top...

--- parser_download_pictures.py
+++ parser_download_pictures.py
@@ -1,67 +1,46 @@
 # """
 # We visit website and download pictures in folder. Let's try
-# https://stock.adobe.com/ru/free
+# 'https://unsplash.com/'
 # """
-# import requests
-# from bs4 import BeautifulSoup
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# import time
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 import urllib.request
 import os
 
-#
-# url = 'https://unsplash.com/'
-# # url = 'https://stock.adobe.com/free'
-# browser = webdriver.Chrome()
-# browser.maximize_window()
-# browser.get(url)
-# time.sleep(2)
-# headers = {
-#     'Accept': '*/*',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-# }
-#
-#
-#
-# def topic_selection(topic_name):
-#     button = browser.find_element(By.XPATH, '//*[@id="app"]/div/div[3]/div[1]/div/div[2]/div/div[1]/div/div/div/div[2]/div[1]/form/div[1]/input')
-#     button.send_keys(topic_name)
-#     time.sleep(1)
-#     # нажимаем enter
-#     button.send_keys(u'\ue007')
-#
-#
-# def take_href():
-#     req = requests.get(url, headers=headers)
-#     soup = BeautifulSoup(req.text, 'lxml')
-#
-#     divs = soup.find('div', class_='search-photos-route')
-#     print(divs)
-#
-#
-# topic_selection('woman')
-# time.sleep(500)
-# browser.close()
 
-
-import requests
-from bs4 import BeautifulSoup
-
-# url = 'https://unsplash.com/'
-# headers = {
-#     'Accept': '*/*',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-# }
-with open('Woman.html', encoding='utf-8') as file:
-    src = file.read()
-# req = requests.get(url, headers=headers)
-# soup = BeautifulSoup(req.text, 'lxml')
-soup = BeautifulSoup(src, 'lxml')
+url = 'https://unsplash.com/'
+browser = webdriver.Chrome()
+browser.maximize_window()
+browser.get(url)
+time.sleep(2)
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+}
 link_list, names = [], []
 
 
+def topic_selection(topic_name):
+    button = browser.find_element(By.XPATH, '//*[@id="app"]/div/div[3]/div[1]/div/div[2]/div/div[1]/div/div/div/div[2]/div[1]/form/div[1]/input')
+    button.send_keys(topic_name)
+    time.sleep(1)
+    # нажимаем enter
+    button.send_keys(u'\ue007')
+
+
+def take_href():
+    req = requests.get(url, headers=headers)
+    soup = BeautifulSoup(req.text, 'lxml')
+    divs = soup.find('div', class_='search-photos-route')
+    print(divs)
+
+
 def collect_names_links():
+    req = requests.get(url, verify=False)
+    soup = BeautifulSoup(req.text, 'lxml')
     divs = soup.find('div', class_='mItv1')
     for div in divs:
         links = div.find_all('img')
@@ -81,3 +60,19 @@ def make_folder(path, folder_name):
 def save_names_links():
     for link, name in zip(link_list, names):
         urllib.request.urlretrieve(link, f'H:\Python\myProjects\pictures\{name}.jpg')
+
+
+topic_selection('woman')
+time.sleep(1)
+# скролим в самый конец странички
+browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+time.sleep(1)
+# скролим наверх
+browser.execute_script(("window.scrollTo(0, document.body.scrollHeight)"))
+time.sleep(1)
+collect_names_links()
+make_folder('H:\Python\myProjects', 'pictures')
+time.sleep(1)
+save_names_links()
+time.sleep(500)
+browser.close()
