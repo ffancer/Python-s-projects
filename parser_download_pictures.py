847f13421472dbaa6be69318f43ec565391b17c1
Paul <gafgafgarion05@gmail.com>
2022-09-22 08:36:51 +0300

    make folder, if folder exist - no error, but save pictures in wrong directory, need fix

--- parser_download_pictures.py
+++ parser_download_pictures.py
@@ -5,6 +5,11 @@ We visit website and download pictures in folder. Let's try
 upd. for ver 1.1:
 - want direct download to folder (without list) (хочу грузить картинки сразу в папку, минуя их сохр. в список)
 - modify the script so that all pictures are saved (бустануть так что бы сохранялись ВСЕ картинки, в 1.00 не все)
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+interesting:
+- from re import sub  -- чтобы удалить буквы из строк типа 'b456G541gh6j' и занимало минимум места в коде
+- button.send_keys(u'\ue007')  -- нажимает [ENTER]
+- os.makedirs(full_path, exist_ok=True) -- exist_ok позволяет или создать папку, а если она создана, то ошибок не возникнет
 """
 import requests
 from bs4 import BeautifulSoup
@@ -15,7 +20,7 @@ import urllib.request
 import os
 import certifi
 import urllib3
-from re import sub
+from re import sub  # чтобы удалить буквы из строк типа 'b456G541gh6j' и занимало минимум места в коде
 
 
 url = 'https://unsplash.com/'
@@ -48,29 +53,11 @@ def take_href():
 
 def collect_names_links():
     req = requests.get(url)
-    # req = requests.get(url, verify=False)
-    # req = requests.get(url, headers=headers)
+
     soup = BeautifulSoup(req.text, 'lxml')
-    # divs = soup.find('div', class_='mItv1')
-    # for div in divs:
-    #     links = div.find_all('img')
-    #     for link in links:
-    #         # получаем название
-    #         name = (link.get('srcset').split()[-2])[28:].split('?')[0]
-    #         names_list.append(name)
-    #         link_list.append(link.get('srcset').split()[-2])
+
     select_images = soup.select("img")
     for nested_images in select_images:
-    #     try:
-    #         for num, image in enumerate(nested_images.get('srcset').split()):
-    #             if image[:3] == 'htt':
-    #                 link_list.append(image)
-    #                 # надо дать имена файлам или найти способ сохранения без этого
-    #                 # name = (image.get('srcset').split()[-2])[28:].split('?')[0]
-    #                 # names_list.append(name)
-    #                 names_list.append(num)
-    #     except AttributeError:
-    #         continue
         for nested_images in select_images:
             try:
                 for i, j in enumerate(nested_images.get('srcset').split()):
@@ -83,15 +70,20 @@ def collect_names_links():
                 continue
 
 
-# # path - 'H:\Python\myProjects'    folder_name - 'pictures'
-# def make_folder(path, folder_name):
-#     full_path = os.path.join(path, folder_name)
-#     os.mkdir(full_path)
-#
-#
+def make_folder(path, folder_name):
+    full_path = os.path.join(path, folder_name)
+    os.makedirs(full_path, exist_ok=True)
+
+
+# def save_names_links():
+#     for link, name in zip(links_list, names_list):
+#         urllib.request.urlretrieve(link, f'H:\Python\myProjects\pictures\{name}.jpg')
+
+
 def save_names_links():
     for link, name in zip(links_list, names_list):
-        urllib.request.urlretrieve(link, f'H:\Python\myProjects\pictures\{name}.jpg')
+        path = make_folder('H:\Python\myProjects', 'pictures')
+        urllib.request.urlretrieve(link, f'{path}{name}.jpg')
 
 
 def scrolling_webpage(scrolling_pixels):
