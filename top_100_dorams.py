b0d0565b203f07099408ed04c894ccc212089f0a
Paul <gafgafgarion05@gmail.com>
2022-09-24 07:56:53 +0300

    -some edits for first site

--- top_100_dorams.py
+++ top_100_dorams.py
@@ -1,5 +1,9 @@
 """
 top 100 dorams from https://vsedoramy.net/top100korean.html
++++++++++++++++++++++++++++++++++++++++++++
+link at google docs with top 100 dorams:
+https://docs.google.com/spreadsheets/d/1pC1hWyYc6YNKary4_NAzG4GQLaaUF5sCGrxToaxIyjU/edit#gid=1182540734
++++++++++++++++++++++++++++++++++++++++++++
 upd. for version 1.02:
 - need more sites (2-3 or more)
 - no duplicates
@@ -14,25 +18,16 @@ headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
 }
-# req = requests.get(url, headers=headers)
-# soup = BeautifulSoup(req.text, 'lxml')
 
-# with open('topdorams.html', 'w', encoding='utf-8') as file:
-#     file.write(req.text)
-
-
-# можно работать с сохраненой страничкой, т.к. динамики на сайте нет
-with open('topdorams.html', encoding='utf-8') as file:
-    src = file.read()
-
-soup = BeautifulSoup(src, 'lxml')
-cards = soup.find('ol', class_='clearfix').find_all('li', class_='top100-item')
+req = requests.get(url, headers=headers)
+soup = BeautifulSoup(req.text, 'lxml')
 # сделал нумерование от 1 до 100 в первом вложенном списке
 data_list = [[i for i in range(1, 101)], [], [], []]
 
 
 # ищем и записываем данные в список
 def data_search():
+    cards = soup.find('ol', class_='clearfix').find_all('li', class_='top100-item')
     for card in cards:
         title = card.find('img', alt=True)
         data_list[1].append(title['alt'])
@@ -51,10 +46,5 @@ def list_to_excel():
 data_search()
 list_to_excel()
 
-"""
-link at google docs with top 100 dorams:
-https://docs.google.com/spreadsheets/d/1pC1hWyYc6YNKary4_NAzG4GQLaaUF5sCGrxToaxIyjU/edit#gid=1182540734
-"""
 
-
-
+# https://doramy.club/top
