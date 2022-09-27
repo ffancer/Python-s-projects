deb5521ceb76df708befa01a567723e2915b3afb
Paul <gafgafgarion05@gmail.com>
2022-09-27 08:40:44 +0300

    in site there are blocks for parsing this way

--- parser_skiddle.com.py
+++ parser_skiddle.com.py
@@ -16,10 +16,23 @@ headers = {
 req = requests.get(url, headers=headers)
 soup = BeautifulSoup(req.text, 'lxml')
 tables = soup.find_all('table')
-# print(tables)
+
+links_list = []
 for table in tables:
-    # for item in table.find_all('a'):
-        # link = 'https://www.skiddle.com/festivals' + item.get('href')
+    for item in table.find_all('a'):
+        link = 'https://www.skiddle.com/festivals' + str(item.get('href'))
+        links_list.append(link)
         # name = item.text
-    for item in table.find_all('td', class_='hide-mobile hide-tablet show-desktop'):
-        date = item.text
+    # for item in table.find_all('td', class_='hide-mobile hide-tablet show-desktop'):
+    #     date = item.text
+
+for url in links_list:
+    req = requests.get(url=url, headers=headers)
+
+    try:
+        soup = BeautifulSoup(req.text, 'lxml')
+        # name = soup.find_all('div', class_='MuiBox-root css-1tglkrx').find('h1', class_='MuiTypography-root MuiTypography-body1 css-r2lffm')
+        # print(name)
+        print(soup)
+    except:
+        print('error')
