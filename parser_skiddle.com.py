80b58634a8747ccd91ea33680d0084c57a197c4a
Paul <gafgafgarion05@gmail.com>
2022-09-27 11:39:58 +0300

    wrong links, my mistakes, need fix

--- parser_skiddle.com.py
+++ parser_skiddle.com.py
@@ -22,22 +22,26 @@ tables = soup.find_all('table')
 links_list = []
 for table in tables:
     for item in table.find_all('a'):
-        link = 'https://www.skiddle.com/festivals' + str(item.get('href'))
-        links_list.append(link)
+        # link = 'https://www.skiddle.com/festivals' + str(item.get('href'))
+        link = item.get('href')
+        print(link)
+        # links_list.append(link)
         # name = item.text
     # for item in table.find_all('td', class_='hide-mobile hide-tablet show-desktop'):
     #     date = item.text
 
-for url in links_list[:2]:
-    req = requests.get(url=url, headers=headers)
-    browser = webdriver.Chrome()
-    browser.maximize_window()
-    browser.get(url)
-    try:
-        soup = BeautifulSoup(req.text, 'lxml')
-        name = soup.find_all('div', class_='MuiBox-root css-1tglkrx').find('h1', class_='MuiTypography-root MuiTypography-body1 css-r2lffm')
-        print(name)
-        print(soup)
-    except:
-        print('error')
-    browser.close()
+print(links_list)
+# for url in links_list[:2]:
+#     req = requests.get(url=url, headers=headers)
+#     browser = webdriver.Chrome()
+#     browser.maximize_window()
+#     browser.get(url)
+#     try:
+#         soup = BeautifulSoup(req.text, 'lxml')
+#         name = soup.find_all('div', class_='MuiBox-root css-1tglkrx').find('h1', class_='MuiTypography-root MuiTypography-body1 css-r2lffm')
+#         print(name)
+#         print(soup)
+#     except:
+#         print('error')
+#     time.sleep(2)
+#     browser.close()
