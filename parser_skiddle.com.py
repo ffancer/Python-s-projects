fb2d7fec7ac7de699679ce3802e1d89a04a97401
Paul <gafgafgarion05@gmail.com>
2022-09-27 11:51:07 +0300

    took names and dates

--- parser_skiddle.com.py
+++ parser_skiddle.com.py
@@ -22,29 +22,30 @@ tables = soup.find_all('table')
 links_list = []
 for table in tables:
     for item in table.find_all('a'):
-        # link = 'https://www.skiddle.com/festivals' + str(item.get('href'))
 
         # collect urls in list
         if item.get('href') is not None:
             links_list.append('https://www.skiddle.com' + item.get('href'))
 
-        # links_list.append(link)
         # name = item.text
     # for item in table.find_all('td', class_='hide-mobile hide-tablet show-desktop'):
     #     date = item.text
 
-print(links_list)
-# for url in links_list[:2]:
-#     req = requests.get(url=url, headers=headers)
-#     browser = webdriver.Chrome()
-#     browser.maximize_window()
-#     browser.get(url)
-#     try:
-#         soup = BeautifulSoup(req.text, 'lxml')
-#         name = soup.find_all('div', class_='MuiBox-root css-1tglkrx').find('h1', class_='MuiTypography-root MuiTypography-body1 css-r2lffm')
-#         print(name)
-#         print(soup)
-#     except:
-#         print('error')
-#     time.sleep(2)
-#     browser.close()
+
+for url in links_list[:2]:
+    req = requests.get(url=url, headers=headers)
+    # browser = webdriver.Chrome()
+    # browser.maximize_window()
+    # browser.get(url)
+    try:
+        soup = BeautifulSoup(req.text, 'lxml')
+        name = soup.find('h1', class_='MuiTypography-root MuiTypography-body1 css-r2lffm').text
+        date = soup.find('div', class_='MuiGrid-root MuiGrid-item MuiGrid-grid-xs-11 css-twt0ol').text
+        print(date)
+        time.sleep(1)
+
+
+    except:
+        print('error')
+    # time.sleep(2)
+    # browser.close()
