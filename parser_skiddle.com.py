c14fd100d174bcd9d20165752ec17fc77defa712
Paul <gafgafgarion05@gmail.com>
2022-09-27 07:35:04 +0300

    take all a-z of festivals 2022

--- parser_skiddle.com.py
+++ parser_skiddle.com.py
@@ -1,8 +1,19 @@
 """
-requests, beautifulsoup, lxml
-собираем карточки фестивалей
-"""
+requests, beautifulsoup, lxml, json
 
+собираем карточки фестивалей который будет джсон файлом
+"""
+import requests
+from bs4 import BeautifulSoup
 
 
 url = 'https://www.skiddle.com/festivals/'
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+}
+
+req = requests.get(url, headers=headers)
+soup = BeautifulSoup(req.text, 'lxml')
+tables = soup.find_all('table')
+print(tables)
