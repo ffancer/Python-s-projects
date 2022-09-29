453b815cb8685f4e1c8235261e7d600a745c4d13
Paul <gafgafgarion05@gmail.com>
2022-09-29 11:03:06 +0300

    add description

--- parser_skiddle.com.py
+++ parser_skiddle.com.py
@@ -21,38 +21,41 @@ soup = BeautifulSoup(req.text, 'lxml')
 tables = soup.find_all('table')
 links_list = []
 lst = []
+
 for table in tables:
     for item in table.find_all('a'):
         # collect urls in list
         if item.get('href') is not None:
             links_list.append('https://www.skiddle.com' + item.get('href'))
-
-for url in links_list[:1]:
+print(links_list)
+for url in links_list[:6]:
+    print(url)
     req = requests.get(url=url, headers=headers)
     try:
         soup = BeautifulSoup(req.text, 'lxml')
         name = soup.find('h1', class_='MuiTypography-root MuiTypography-body1 css-r2lffm').text
         date = soup.find('div', class_='MuiGrid-root MuiGrid-item MuiGrid-grid-xs-11 css-twt0ol').text
+        description = soup.find('div', class_='MuiBox-root css-1ebprri').text[:-10]
         print(name, date)
         browser = webdriver.Chrome()
         browser.get(url)
-
         browser.find_element(By.XPATH, '//*[@id="panel2873bh-header"]/div[2]').click()
         # more detailed location
         # location = browser.find_element(By.XPATH, '//*[@id="panel2873bh-content"]/div/div/div').text.split('\n')
         location = browser.find_element(By.XPATH, '//*[@id="panel2873bh-content"]/div/div/div').text
         # name = browser.find_element(By.XPATH, '//*[@id="__next"]/div/div[3]/h1').text
-
         print(location)
-
-        browser.close()
         lst.append(
             {
                 'name': name,
                 'date': date,
+                'description': description,
                 'location': location
             }
         )
+        print(lst)
+        browser.close()
+
     except:
         print('error')
 
