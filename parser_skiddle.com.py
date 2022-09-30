a1b156b0ca86d1ad127de5b6c338d598345336b7
Paul <gafgafgarion05@gmail.com>
2022-09-30 12:29:52 +0300

    final version 1.0

--- parser_skiddle.com.py
+++ parser_skiddle.com.py
@@ -5,9 +5,6 @@ requests, beautifulsoup, lxml, json
 """
 import requests
 from bs4 import BeautifulSoup
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import json
 
 
@@ -16,11 +13,13 @@ headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
 }
+
 req = requests.get(url, headers=headers)
 soup = BeautifulSoup(req.text, 'lxml')
 tables = soup.find_all('table')
 links_list = []
-lst = []
+json_list = []
+
 
 for table in tables:
     for item in table.find_all('a'):
@@ -28,58 +27,27 @@ for table in tables:
         if item.get('href') is not None:
             links_list.append('https://www.skiddle.com' + item.get('href'))
 
-for url in links_list[:4]:
+for url in links_list:
     print(url)
     req = requests.get(url=url, headers=headers)
     soup = BeautifulSoup(req.text, 'lxml')
+
     name = soup.find('h1', class_='MuiTypography-root MuiTypography-body1 css-r2lffm').text
     date = soup.find('div', class_='MuiGrid-root MuiGrid-item MuiGrid-grid-xs-11 css-twt0ol').text
     description = soup.find('div', class_='MuiBox-root css-1ebprri').text[:-10]
     image = soup.find(class_='css-1x26sxc')['data']
-    # print(name, date, description)
+    ticket = url + '#tickets'
 
-    #     # soup = BeautifulSoup(req.text, 'lxml')
-    #
-    #
-    #     # browser = webdriver.Chrome()
-    #     # browser.get(url)
-    #     # browser.find_element(By.XPATH, '//*[@id="panel2873bh-header"]/div[2]').click()
-    #     # location = browser.find_element(By.XPATH, '//*[@id="panel2873bh-content"]/div/div/div').text
-    cards = soup.find('div', class_='MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation1 css-ckmrqz')
-    for card in cards:
-        try:
+    json_list.append(
+        {
+            'Url': url,
+            'Name': name,
+            'Date': date,
+            'Description': description,
+            "Festival's image": image,
+            'Buy ticket': ticket
+        }
+    )
 
-            place = card.find_all('span')
-            data = [i.text.strip() for i in place]
-            place = data[2]
-            price = data[-2]
-            image = card.get('data')
-            if '£' not in price:
-                price = 'Free'
-            if image is None:
-                continue
-
-            print(image, place, price)
-            # lst.append(
-            #     {
-            #         'url': url,
-            #         'name': name,
-            #         'image url': image,
-            #         'date': date,
-            #         'description': description,
-            #         'place': place,
-            #         'price': price
-            #     }
-            # )
-        except IndexError:
-            continue
-
-
-
-        # browser.close()
-
-    # except:
-    #     print('error')
-
-# with open('festival.json', 'a', encoding='utf-8') as file:
-#     json.dump(lst, file, indent=4, ensure_ascii=False)
+with open('festivals.json', 'a', encoding='utf-8') as file:
+    json.dump(json_list, file, indent=4, ensure_ascii=False)
