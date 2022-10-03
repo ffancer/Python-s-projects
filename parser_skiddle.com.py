4bf00eb5c369e399445181a9b64a73b80c2247ad
Paul <gafgafgarion05@gmail.com>
2022-10-03 10:06:47 +0300

    made 'def collect_links'

--- parser_skiddle.com.py
+++ parser_skiddle.com.py
@@ -18,40 +18,47 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
 }
 
-req = requests.get(url, headers=headers)
-soup = BeautifulSoup(req.text, 'lxml')
-tables = soup.find_all('table')
-links_list = []
-json_list = []
 
 
-for table in tables:
-    for item in table.find_all('a'):
-        # collect urls in list
-        if item.get('href') is not None:
-            links_list.append('https://www.skiddle.com' + item.get('href'))
-
-for url in links_list:
-    print(url)
-    req = requests.get(url=url, headers=headers)
+def collect_links():
+    req = requests.get(url, headers=headers)
     soup = BeautifulSoup(req.text, 'lxml')
+    tables = soup.find_all('table')
+    links_list = []
 
-    name = soup.find('h1', class_='MuiTypography-root MuiTypography-body1 css-r2lffm').text
-    date = soup.find('div', class_='MuiGrid-root MuiGrid-item MuiGrid-grid-xs-11 css-twt0ol').text
-    description = soup.find('div', class_='MuiBox-root css-1ebprri').text[:-10]
-    image = soup.find(class_='css-1x26sxc')['data']
-    ticket = url + '#tickets'
+    for table in tables:
+        for item in table.find_all('a'):
+            # collect urls in list
+            if item.get('href') is not None:
+                links_list.append('https://www.skiddle.com' + item.get('href'))
 
-    json_list.append(
-        {
-            'Url': url,
-            'Name': name,
-            'Date': date,
-            'Description': description,
-            "Festival's image": image,
-            'Buy ticket': ticket
-        }
-    )
+    return links_list
 
-with open('festivals.json', 'a', encoding='utf-8') as file:
-    json.dump(json_list, file, indent=4, ensure_ascii=False)
+# json_list = []
+# for url in links_list:
+#     print(url)
+#     req = requests.get(url=url, headers=headers)
+#     soup = BeautifulSoup(req.text, 'lxml')
+#
+#     name = soup.find('h1', class_='MuiTypography-root MuiTypography-body1 css-r2lffm').text
+#     date = soup.find('div', class_='MuiGrid-root MuiGrid-item MuiGrid-grid-xs-11 css-twt0ol').text
+#     description = soup.find('div', class_='MuiBox-root css-1ebprri').text[:-10]
+#     image = soup.find(class_='css-1x26sxc')['data']
+#     ticket = url + '#tickets'
+#
+#     json_list.append(
+#         {
+#             'Url': url,
+#             'Name': name,
+#             'Date': date,
+#             'Description': description,
+#             "Festival's image": image,
+#             'Buy ticket': ticket
+#         }
+#     )
+#
+# with open('festivals.json', 'a', encoding='utf-8') as file:
+#     json.dump(json_list, file, indent=4, ensure_ascii=False)
+
+
+print(collect_links())
