e1ac63b4595a94341e6cbf3781a897d1478f8d22
Paul <gafgafgarion05@gmail.com>
2022-10-24 10:03:23 +0300

    'clear' bs and requests didn't work, need to use other

--- parser_flats_list.py
+++ parser_flats_list.py
@@ -1,4 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
+URL = 'https://spb.domclick.ru/'
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+}
 
+req = requests.get(url=URL, headers=headers)
+soup = BeautifulSoup(req.text, 'lxml')
+
+print(soup)
