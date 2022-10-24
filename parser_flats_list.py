721b4c3f993d6255b9b54ccc7544234107ceca6a
Paul <gafgafgarion05@gmail.com>
2022-10-24 10:36:25 +0300

    -

--- parser_flats_list.py
+++ parser_flats_list.py
@@ -5,15 +5,24 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# URL = 'https://spb.domclick.ru'
-URL = 'https://magnit.ru'
+URL = 'https://domclick.ru'
+# URL = 'https://magnit.ru'
 headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
 }
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-blink-features")
+options.add_argument("--disable-blink-features=AutomationControlled")
+# relevant part ends here
+driver = webdriver.Chrome(executable_path=r"chromedriver.exe", options=options)
+driver.maximize_window()
+driver.get(URL)
 
-browser = webdriver.Chrome()
-browser.maximize_window()
-browser.get(URL)
+# browser = webdriver.Chrome(options=options)
+# browser.maximize_window()
+# browser.get(URL)
 time.sleep(60)
-browser.close()
+# browser.close()
+# chrome_driver.get(URL)
+# chrome_driver.close()
