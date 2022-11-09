f4c64b6b1d52a5aed06ccf3ed4d0f9668740b2d5
Paul <gafgafgarion05@gmail.com>
2022-11-09 09:35:44 +0300

    good for parse, no guard

--- parser_list_anime.py
+++ parser_list_anime.py
@@ -1,4 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = 'https://myanimelist.net/topanime.php'
 
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+}
+
+req = requests.get(url, headers=headers)
+soup = BeautifulSoup(req.text, 'lxml')
+print(soup)
