98168360a9faa87fcf9f5273be66f11ab1e56c07
Paul <gafgafgarion05@gmail.com>
2022-11-10 08:58:24 +0300

    -

--- parser_list_anime.py
+++ parser_list_anime.py
@@ -10,23 +10,36 @@ headers = {
 
 req = requests.get(url, headers=headers)
 soup = BeautifulSoup(req.text, 'lxml')
-all_info_cards = soup.find_all('table')[0].find_all('tr')
-info_card = [i.text.replace('\n', '').strip() for i in all_info_cards]
-json_list = []
+# first
+# all_info_cards = soup.find_all('table')[0].find_all('tr')
+# info_card = [i.text.replace('\n', '').strip() for i in all_info_cards]
+# json_list = []
+#
+# for card in info_card:
+#     card = card.split('      ')
+#     if len(card) < 2:
+#         continue
+#     else:
+#         json_list.append(
+#             {
+#                 'name': card[0],
+#                 'how many episodes': card[1],
+#                 'when come out': card[2],
+#                 'score': card[4]
+#             }
+#         )
+# print(json_list)
 
-for card in info_card:
-    card = card.split('      ')
-    if len(card) < 2:
-        continue
-    else:
-        json_list.append(
-            {
-                'name': card[0],
-                'how many episodes': card[1],
-                'when come out': card[2],
-                'score': card[4]
-            }
-        )
+
+# second
+# all_info = soup.find_all('div', class_='di-ib clearfix')
+# for i,j in enumerate(all_info):
+#     print(i, j.h3)
 
 
-print(json_list)
+# third
+all_info = soup.find_all('tr', class_='ranking-list')
+# for info in all_info:
+#     print(info)
+info = [i.text.replace('\n', '').strip() for i in all_info]
+print(info)
