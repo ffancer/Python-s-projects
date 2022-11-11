527f31249ab621eec82acb26fb33d6133a9c76c3
Paul <gafgafgarion05@gmail.com>
2022-11-11 11:44:30 +0300

    final version 1.00

--- parser_list_anime.py
+++ parser_list_anime.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import pandas as pd
-
+import time
 
 headers = {
     'Accept': '*/*',
@@ -10,44 +10,50 @@ headers = {
 }
 
 
-# def collect_data():
-#     json_list = []
-#
-#     for i in range(0, 150, 50):
-#         url = f'https://myanimelist.net/topanime.php?limit={i}'
-#         req = requests.get(url, headers=headers)
-#         soup = BeautifulSoup(req.text, 'lxml')
-#         all_cards = soup.find_all(class_='ranking-list')
-#
-#         for card in all_cards:
-#             rank = card.find(class_='rank ac').text.strip()
-#             anime_description = card.find(class_='title al va-t word-break').text.split('\n')[7:]
-#             name = anime_description[0]
-#             number_of_episodes = anime_description[1].strip()
-#             release_date = anime_description[2].strip()
-#             score = card.find(class_='score ac fs14').text.strip()
-#
-#             json_list.append(
-#                 {
-#                     'Anime rank': rank,
-#                     'Name of the title': name,
-#                     'Release': release_date,
-#                     'Number of episodes': number_of_episodes,
-#                     'Score': score
-#                 }
-#             )
-#
-#     return json_list
-#
-#
-# def save_json_file():
-#     with open('top 1000 anime.json', 'a', encoding='utf-8') as file:
-#         json.dump(collect_data(), file, indent=4, ensure_ascii=False)
+def collect_data():
+    json_list = []
+
+    for i in range(0, 1000, 50):
+        url = f'https://myanimelist.net/topanime.php?limit={i}'
+        time.sleep(1)
+        req = requests.get(url, headers=headers)
+        soup = BeautifulSoup(req.text, 'lxml')
+        all_cards = soup.find_all(class_='ranking-list')
+
+        for card in all_cards:
+            rank = card.find(class_='rank ac').text.strip()
+            anime_description = card.find(class_='title al va-t word-break').text.split('\n')[7:]
+            name = anime_description[0]
+            number_of_episodes = anime_description[1].strip()
+            release_date = anime_description[2].strip()
+            score = card.find(class_='score ac fs14').text.strip()
+
+            json_list.append(
+                {
+                    'Anime rank': rank,
+                    'Name of the title': name,
+                    'Release': release_date,
+                    'Number of episodes': number_of_episodes,
+                    'Score': score
+                }
+            )
+
+    return json_list
 
 
-# save_json_file()
+def save_json_file():
+    with open('top 1000 anime.json', 'a', encoding='utf-8') as file:
+        json.dump(collect_data(), file, indent=4, ensure_ascii=False)
 
 
 def from_json_to_excel():
-    with open('top 1000 anime.json') as json_file:
+    with open('top 1000 anime.json', encoding='utf-8') as json_file:
         data = json.load(json_file)
+
+    df = pd.DataFrame(data)
+    return df.to_excel('top 1000 anime.xlsx')
+
+
+save_json_file()
+time.sleep(1)
+from_json_to_excel()
