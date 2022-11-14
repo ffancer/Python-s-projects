18736df8f3cbe29331fb78f4f3ab0ecfc0fbbaf8
Paul <gafgafgarion05@gmail.com>
2022-11-14 16:56:33 +0300

    -

--- parser_top_films_imdb.py
+++ parser_top_films_imdb.py
@@ -1,1 +1,14 @@
-url = 'https://www.imdb.com/chart/top/'
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = 'https://www.imdb.com/chart/top/'
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+}
+
+req = requests.get(URL, headers=headers)
+soup = BeautifulSoup(req.text, 'lxml')
+
+print(soup)
