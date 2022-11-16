d988b61b7809719dc7bcb0e83b1fb1a745859145
Paul <gafgafgarion05@gmail.com>
2022-11-16 10:30:16 +0300

    error coz incorrect json file, troubles with 'rating' I think

--- parser_top_films_imdb.py
+++ parser_top_films_imdb.py
@@ -5,6 +5,7 @@ IMDb Top 250 Movies
 import requests
 from bs4 import BeautifulSoup
 import json
+import pandas as pd
 
 
 URL = 'https://www.imdb.com/chart/top/'
@@ -70,4 +71,13 @@ def save_json_file():
         json.dump(lst, file, indent=4, ensure_ascii=False)
 
 
+def from_json_to_excel():
+    with open('top 250 films.json', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+
+    df = pd.DataFrame(data)
+    return df.to_excel('top 250 films.xlsx')
+
+
 save_json_file()
+from_json_to_excel()
