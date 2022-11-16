1238855ff747bf91043a20accd5d088c0977d262
Paul <gafgafgarion05@gmail.com>
2022-11-16 09:57:57 +0300

    -

--- parser_top_films_imdb.py
+++ parser_top_films_imdb.py
@@ -38,28 +38,25 @@ all_about_film = soup.find_all(class_='lister-list')
 #     print(place.text.strip())
 
 
-def take_rating():
-    lst = []
-    rating = soup.find_all(class_='ratingColumn imdbRating')
-    for j in rating:
-        lst.append(j.text.strip())
-    return lst
+# def take_rating():
+#     lst = []
+#     rating = soup.find_all(class_='ratingColumn imdbRating')
+#     for j in rating:
+#         lst.append(j.text.strip())
+#     return lst
+#
+#
+# for film in all_about_film:
+#     film_name = film.find_all(class_='titleColumn')
+#     for i in film_name:
+#         json_list.append(
+#             {
+#                 'Place': i.text.split()[0],
+#                 'Name': ' '.join(i.text.split()[1:-1]),
+#                 'Year': i.text.split()[-1],
+#             }
+#         )
+# for (x, y) in zip(json_list, take_rating()):
+#     print(x, y)
 
-
-for film in all_about_film:
-    film_name = film.find_all(class_='titleColumn')
-    for i in film_name:
-        json_list.append(
-            {
-                'Place': i.text.split()[0],
-                'Name': ' '.join(i.text.split()[1:-1]),
-                'Year': i.text.split()[-1],
-            }
-        )
-
-# print(json_list)
-# print(take_rating())
-
-for (x, y) in zip(json_list, take_rating()):
-    print(x, y)
-
+print(soup.find_all('tr'))
