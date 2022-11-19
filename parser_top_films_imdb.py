f8e2b658b3d3cd1768d2888c8bc136df08884ac1
Paul <gafgafgarion05@gmail.com>
2022-11-19 12:43:17 +0300

    from json to excel

--- parser_top_films_imdb.py
+++ parser_top_films_imdb.py
@@ -16,56 +16,38 @@ json_list = []
 req = requests.get(URL, headers=headers)
 soup = BeautifulSoup(req.text, 'lxml')
 
-# films = soup.find(class_='lister-list').find_all('tr')
-# #
-# for film in films:
-#     name = film.find('td', class_='titleColumn').a.text
-#     rank = film.find('td', class_='titleColumn').text.strip().split('.')[0]
-#     year = film.find('td', class_='titleColumn').span.text.replace('(', '').replace(')', '')
-#     rating = film.find('td', class_='ratingColumn imdbRating').strong.text
-#     print(rating)
 
-
-
-# lst = []
-# rating = soup.find_all(class_='ratingColumn imdbRating')
-# for place in rating:
-#     lst.append(place.text.strip())
+# def collecting_info():
+#     films = soup.find(class_='lister-list').find_all('tr')
 #
+#     for film in films:
+#         place = film.find(class_='titleColumn').text.split('.')[0].strip()
+#         name = film.find(class_='titleColumn').a.text
+#         year = film.find(class_='titleColumn').span.text.strip('()')
+#         rating = film.find(class_='ratingColumn imdbRating').strong.text
+#         json_list.append(
+#             {
+#                 'Place': place,
+#                 'Name': name,
+#                 'Year': year,
+#                 'IMDb Rating': rating
+#             }
+#         )
 #
+#     return json_list
 
 
-def collecting_info():
-    films = soup.find(class_='lister-list').find_all('tr')
-
-    for film in films:
-        place = film.find(class_='titleColumn').text.split('.')[0].strip()
-        name = film.find(class_='titleColumn').a.text
-        year = film.find(class_='titleColumn').span.text.strip('()')
-        rating = film.find(class_='ratingColumn imdbRating').strong.text
-        json_list.append(
-            {
-                'Place': place,
-                'Name': name,
-                'Year': year,
-                'IMDb Rating': rating
-            }
-        )
-
-    return json_list
+# def save_json_file():
+#     with open('top 250 films.json', 'a', encoding='utf-8') as file:
+#         json.dump(collecting_info(), file, indent=4, ensure_ascii=False)
 
 
-def save_json_file():
-    with open('top 250 films.json', 'a', encoding='utf-8') as file:
-        json.dump(collecting_info(), file, indent=4, ensure_ascii=False)
+def from_json_to_excel():
+    with open('top 250 films.json', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+    df = pd.DataFrame(data)
+    return df.to_excel('top 250 films.xlsx')
 
 
-# def from_json_to_excel():
-#     with open('top 250 films.json', encoding='utf-8') as json_file:
-#         data = json.load(json_file)
-#     df = pd.DataFrame(data)
-#     return df.to_excel('top 250 films.xlsx')
-
-
-save_json_file()
-# from_json_to_excel()
+# save_json_file()
+from_json_to_excel()
