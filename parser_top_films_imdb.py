b1e8f52517b88eaa3bb4d3b93ddb20fbce41c681
Paul <gafgafgarion05@gmail.com>
2022-11-19 12:44:41 +0300

    basic parser version 1.00

--- parser_top_films_imdb.py
+++ parser_top_films_imdb.py
@@ -6,6 +6,8 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import pandas as pd
+import time
+
 
 URL = 'https://www.imdb.com/chart/top/'
 headers = {
@@ -17,29 +19,30 @@ req = requests.get(URL, headers=headers)
 soup = BeautifulSoup(req.text, 'lxml')
 
 
-# def collecting_info():
-#     films = soup.find(class_='lister-list').find_all('tr')
-#
-#     for film in films:
-#         place = film.find(class_='titleColumn').text.split('.')[0].strip()
-#         name = film.find(class_='titleColumn').a.text
-#         year = film.find(class_='titleColumn').span.text.strip('()')
-#         rating = film.find(class_='ratingColumn imdbRating').strong.text
-#         json_list.append(
-#             {
-#                 'Place': place,
-#                 'Name': name,
-#                 'Year': year,
-#                 'IMDb Rating': rating
-#             }
-#         )
-#
-#     return json_list
+def collecting_info():
+    films = soup.find(class_='lister-list').find_all('tr')
+
+    for film in films:
+        place = film.find(class_='titleColumn').text.split('.')[0].strip()
+        name = film.find(class_='titleColumn').a.text
+        year = film.find(class_='titleColumn').span.text.strip('()')
+        rating = film.find(class_='ratingColumn imdbRating').strong.text
+        json_list.append(
+            {
+                'Place': place,
+                'Name': name,
+                'Year': year,
+                'IMDb Rating': rating
+            }
+        )
+
+    return json_list
 
 
-# def save_json_file():
-#     with open('top 250 films.json', 'a', encoding='utf-8') as file:
-#         json.dump(collecting_info(), file, indent=4, ensure_ascii=False)
+def save_json_file():
+    time.sleep(1)
+    with open('top 250 films.json', 'a', encoding='utf-8') as file:
+        json.dump(collecting_info(), file, indent=4, ensure_ascii=False)
 
 
 def from_json_to_excel():
@@ -49,5 +52,6 @@ def from_json_to_excel():
     return df.to_excel('top 250 films.xlsx')
 
 
-# save_json_file()
+save_json_file()
+time.sleep(1)
 from_json_to_excel()
