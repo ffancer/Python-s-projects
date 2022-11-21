4b03272fa8c82ab37afcc03f5ace1ebe61a260e1
Paul <gafgafgarion05@gmail.com>
2022-11-21 13:28:19 +0300

    want to take links from all films

--- parser_top_films_imdb.py
+++ parser_top_films_imdb.py
@@ -1,5 +1,6 @@
 """
 IMDb Top 250 Movies
+for ver 1.1: 1)wanna add directors
 """
 
 import requests
@@ -19,39 +20,52 @@ req = requests.get(URL, headers=headers)
 soup = BeautifulSoup(req.text, 'lxml')
 
 
-def collecting_info():
-    films = soup.find(class_='lister-list').find_all('tr')
+# def collecting_info():
+#     films = soup.find(class_='lister-list').find_all('tr')
+#
+#     for film in films:
+#         place = film.find(class_='titleColumn').text.split('.')[0].strip()
+#         name = film.find(class_='titleColumn').a.text
+#         year = film.find(class_='titleColumn').span.text.strip('()')
+#         rating = film.find(class_='ratingColumn imdbRating').strong.text
+#         json_list.append(
+#             {
+#                 'Place': place,
+#                 'Name': name,
+#                 'Year': year,
+#                 'IMDb Rating': rating
+#             }
+#         )
+#
+#     return json_list
+#
+#
+# def save_json_file():
+#     time.sleep(1)
+#     with open('top 250 films.json', 'a', encoding='utf-8') as file:
+#         json.dump(collecting_info(), file, indent=4, ensure_ascii=False)
+#
+#
+# def from_json_to_excel():
+#     with open('top 250 films.json', encoding='utf-8') as json_file:
+#         data = json.load(json_file)
+#     df = pd.DataFrame(data)
+#     return df.to_excel('top 250 films.xlsx')
+#
+#
+# save_json_file()
+# time.sleep(1)
+# from_json_to_excel()
 
-    for film in films:
-        place = film.find(class_='titleColumn').text.split('.')[0].strip()
-        name = film.find(class_='titleColumn').a.text
-        year = film.find(class_='titleColumn').span.text.strip('()')
-        rating = film.find(class_='ratingColumn imdbRating').strong.text
-        json_list.append(
-            {
-                'Place': place,
-                'Name': name,
-                'Year': year,
-                'IMDb Rating': rating
-            }
-        )
-
-    return json_list
 
 
-def save_json_file():
-    time.sleep(1)
-    with open('top 250 films.json', 'a', encoding='utf-8') as file:
-        json.dump(collecting_info(), file, indent=4, ensure_ascii=False)
 
 
-def from_json_to_excel():
-    with open('top 250 films.json', encoding='utf-8') as json_file:
-        data = json.load(json_file)
-    df = pd.DataFrame(data)
-    return df.to_excel('top 250 films.xlsx')
 
 
-save_json_file()
-time.sleep(1)
-from_json_to_excel()
+
+films = soup.find(class_='lister-list').find_all('tr')
+
+for film in films:
+    link = film.find(class_='titleColumn').get('a href')
+    print(link)
