f9e2f0e153b544e02778e0bf43d97e59a4a4feb2
Paul <gafgafgarion05@gmail.com>
2022-12-12 09:21:37 +0300

    soup made

--- parser_music_groups.py
+++ parser_music_groups.py
@@ -1,3 +1,17 @@
 """
 Будем собирать данные про музыкальные группы из США
 """
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://www.last.fm/ru/tag/rock/artists'
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+}
+
+
+req = requests.get(url, headers=headers)
+soup = BeautifulSoup(req.text, 'lxml')
+print(soup)
