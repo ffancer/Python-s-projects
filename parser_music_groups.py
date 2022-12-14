79fd255547f74c178872aee14b125bef4be92a7f
Paul <gafgafgarion05@gmail.com>
2022-12-14 16:04:42 +0300

    -

--- parser_music_groups.py
+++ parser_music_groups.py
@@ -15,7 +15,7 @@ headers = {
 req = requests.get(url, headers=headers)
 soup = BeautifulSoup(req.text, 'lxml')
 all_cards = soup.find(class_='big-artist-list')
-print(all_cards.li)
+print(all_cards.li.div)
 # cnt = 0
 # while cnt < 10:
 #     for card in all_cards:
