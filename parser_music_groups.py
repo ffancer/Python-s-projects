d471c0f19caceec313a6068bdae9ee5816f78e28
Paul <gafgafgarion05@gmail.com>
2022-12-15 10:07:44 +0300

    problems with 'listeners_count', no int or float but some like xa0649 etc.

--- parser_music_groups.py
+++ parser_music_groups.py
@@ -4,13 +4,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 url = 'https://www.last.fm/ru/tag/rock/artists'
 headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
 }
-
+json_list = []
 
 req = requests.get(url, headers=headers)
 soup = BeautifulSoup(req.text, 'lxml')
@@ -18,6 +17,14 @@ all_cards = soup.find_all(class_='big-artist-list')
 # print(all_cards.li.div)
 
 for card in all_cards:
-    group_name = card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-title').text
-    listeners_count = card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-listeners').text.replace('слушателей', '').strip()
-    short_description = card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-bio').p.text
+    json_list.append(
+        {
+            'group_name': card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-title').text,
+            'listeners_count': card.find(class_='big-artist-list-wrap').find(
+                class_='big-artist-list-listeners').text.replace(
+                'слушателей', '').strip(),
+            'short_description': card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-bio').p.text
+        }
+    )
+
+print(json_list)
