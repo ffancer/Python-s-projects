31bc76cb92035162b49d776cf5f915bec540a451
Paul <gafgafgarion05@gmail.com>
2022-12-16 10:44:31 +0300

    took all info in list

--- parser_music_groups.py
+++ parser_music_groups.py
@@ -4,53 +4,34 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.last.fm/ru/tag/rock/artists'  # https://www.last.fm/ru/tag/rock/artists?page=23
 headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
 }
 json_list = []
 
-req = requests.get(url, headers=headers)
-soup = BeautifulSoup(req.text, 'lxml')
-all_cards = soup.find_all(class_='big-artist-list')
+for k in range(1, 24):
+    url = f'https://www.last.fm/ru/tag/rock/artists?page={k}'  # https://www.last.fm/ru/tag/rock/artists?page=23
+    req = requests.get(url, headers=headers)
+    soup = BeautifulSoup(req.text, 'lxml')
+    music_groups_info = soup.find_all('div', class_='big-artist-list-item js-link-block link-block')
 
-# for card in all_cards:
-#     lst = []
-#     string_listeners_count = card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-listeners').text.strip().replace('слушателей', '')
-#     digit_listeners_count = ''
-#     for i in string_listeners_count:
-#         if i.isdigit():
-#             digit_listeners_count += i
-#
-#     json_list.append(
-#         {
-#             'group_name': card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-title').text,
-#             'listeners_count': int(digit_listeners_count),
-#             'short_description': card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-bio').p.text
-#         }
-#     )
-#
-# print(json_list)
+    for i in range(21):
+        try:
+            json_list.append(
+                {
+                    'название группы': music_groups_info[i].find('h3').text,
+                    'число фанатов': int(
+                        music_groups_info[i].find(class_='big-artist-list-listeners').text.strip().replace('слушатель',
+                                                                                                           '').replace(
+                            'слушателей', '').replace('слушателя', '').replace(' ', '')),
+                    'краткое описание': music_groups_info[i].find(class_='big-artist-list-bio').p.text
 
-# music_groups_info = soup.find(class_='big-artist-list').find_all('li')
-# for music_group in music_groups_info:
-#     print(music_group.find(class_='big-artist-list-title'))
-
-music_groups_info = soup.find_all('div', class_='big-artist-list-item js-link-block link-block')
-
-for i in range(21):
-
-    json_list.append(
-        {
-            'название группы': music_groups_info[i].find('h3').text,
-            'число фанатов': int(music_groups_info[i].find(class_='big-artist-list-listeners').text.strip().replace('слушатель', '').replace('слушателей', '').replace('слушателя', '').replace(' ', '')),
-            'краткое описание': music_groups_info[i].find(class_='big-artist-list-bio').p.text
-        }
-    )
-
-
+                }
+            )
+        except AttributeError:
+            json_list.append(
+                {
+                    'краткое описание': 'Вики-статьи пока нет...'
+                })
 print(json_list)
-
-
-
