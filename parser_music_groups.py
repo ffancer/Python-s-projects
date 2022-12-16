0dcb3f836f2b5384aab722048659cbe233fda9cb
Paul <gafgafgarion05@gmail.com>
2022-12-16 09:40:47 +0300

    -

--- parser_music_groups.py
+++ parser_music_groups.py
@@ -14,23 +14,24 @@ json_list = []
 req = requests.get(url, headers=headers)
 soup = BeautifulSoup(req.text, 'lxml')
 all_cards = soup.find_all(class_='big-artist-list')
-# print(all_cards.li.div)
 
-for card in all_cards:
-    lst = []
-    string_listeners_count = card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-listeners').text.strip().replace('слушателей', '')
-    digit_listeners_count = ''
-    for i in string_listeners_count:
-        if i.isdigit():
-            digit_listeners_count += i
 
-    json_list.append(
-        {
-            'group_name': card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-title').text,
-            'listeners_count': int(digit_listeners_count),
-            'short_description': card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-bio').p.text
-        }
-    )
+# for card in all_cards:
+#     lst = []
+#     string_listeners_count = card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-listeners').text.strip().replace('слушателей', '')
+#     digit_listeners_count = ''
+#     for i in string_listeners_count:
+#         if i.isdigit():
+#             digit_listeners_count += i
 #
-print(json_list)
-# print(lst)
+#     json_list.append(
+#         {
+#             'group_name': card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-title').text,
+#             'listeners_count': int(digit_listeners_count),
+#             'short_description': card.find(class_='big-artist-list-wrap').find(class_='big-artist-list-bio').p.text
+#         }
+#     )
+#
+# print(json_list)
+
+print(type(all_cards))
