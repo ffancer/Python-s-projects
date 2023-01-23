991339511f2674061266fa890a32c2f7b4a1e8a6
Paul <gafgafgarion05@gmail.com>
2023-01-23 09:40:26 +0300

    -

--- parset_cats.py
+++ parset_cats.py
@@ -1,5 +1,16 @@
 """
 Соберем породы кошек в список
 """
+import requests
+from bs4 import BeautifulSoup
+
 
 URL = 'https://hvost.news/animals/cats-breeds/'
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+}
+
+req = requests.get(URL, headers=headers)
+print(req)
+soup = BeautifulSoup(req.text, 'lxml')
