f5cb2118f0e6d41513fef95c9d6ce6b6349a9032
Paul <gafgafgarion05@gmail.com>
2023-01-25 10:14:28 +0300

    -

--- parset_cats.py
+++ parset_cats.py
@@ -14,7 +14,11 @@ headers = {
 req = requests.get(URL, headers=headers)
 soup = BeautifulSoup(req.text, 'lxml')
 # all_cats = soup.find(class_='breeds-list__items')
-all_cats = soup.find_all(class_='pet-page__main')
+
+# all_cats = soup.find_all(class_='pet-page__main')
+# for cat in all_cats:
+#     print(cat.find_all(class_='breeds-list-i__name'))
+
+all_cats = soup.find_all('div', class_='breeds-list__items')
 for cat in all_cats:
-    print(cat)
-    print(cat.find_all(class_='breeds-list-i__name'))
+    print(cat.find('a', class_='breeds-list-i')['href'])
