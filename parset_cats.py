830d6c3544008475c017d43c9cad76c585791f24
Paul <gafgafgarion05@gmail.com>
2023-01-26 11:36:14 +0300

    final version 1.00

--- parset_cats.py
+++ parset_cats.py
@@ -13,42 +13,39 @@ headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
 }
-
 req = requests.get(URL, headers=headers)
 soup = BeautifulSoup(req.text, 'lxml')
 
 
-# def collect_data():
-#     json_list = []
-#     all_cats = soup.find_all('a', class_='breeds-list-i')
-#
-#     for cat in all_cats:
-#         breed = cat.find(class_='breeds-list-i__name').text.strip()
-#         character = cat.find(class_='breeds-list-i__label').text
-#         link = 'https://hvost.news/' + cat['href']
-#         json_list.append(
-#             {
-#                 'Порода': breed,
-#                 'Активность': character,
-#                 'Более подробная информация по ссылке': link
-#             }
-#         )
-#
-#     return json_list
+def collect_data():
+    json_list = []
+    all_cats = soup.find_all('a', class_='breeds-list-i')
+    for cat in all_cats:
+        breed = cat.find(class_='breeds-list-i__name').text.strip()
+        character = cat.find(class_='breeds-list-i__label').text
+        link = 'https://hvost.news/' + cat['href']
+        json_list.append(
+            {
+                'Порода': breed,
+                'Активность': character,
+                'Более подробная информация по ссылке': link
+            }
+        )
+    return json_list
 
 
-# def save_json_file():
-#     with open('cat_breeds.json', 'a', encoding='utf-8') as file:
-#         json.dump(collect_data(), file, indent=4, ensure_ascii=False)
+def save_json_file():
+    with open('cat_breeds.json', 'a', encoding='utf-8') as file:
+        json.dump(collect_data(), file, indent=4, ensure_ascii=False)
 
 
 def from_json_to_excel():
     with open('cat_breeds.json', encoding='utf-8') as json_file:
         data = json.load(json_file)
-
     df = pd.DataFrame(data)
     return df.to_excel('cat_breeds.xlsx')
 
 
+save_json_file()
 from_json_to_excel()
 
