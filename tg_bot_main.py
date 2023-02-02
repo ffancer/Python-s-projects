f8bbe54be2c6a70ef986d5fcbfeb082628f67d61
Paul <gafgafgarion05@gmail.com>
2023-02-02 11:53:01 +0300

    -

--- tg_bot_main.py
+++ tg_bot_main.py
@@ -23,8 +23,41 @@ print('test')
 # #
 # # print(test(4, 5))
 
-def get_tuple(lst: Union[float, bool]) -> [int]:
-    return (int(num) for num in lst)
 
-print(get_tuple([1.0, False, 2.3]))
+# from dataclasses import dataclass
+#
+#
+# @dataclass
+# class DatabaseConfig:
+#     db_host: str       # URL-адрес базы данных
+#     db_user: str       # Username пользователя базы данных
+#     db_password: str   # Пароль к базе данных
+#     database: str      # Название базы данных
+#
+#
+# @dataclass
+# class TgBot:
+#     token: str             # Токен для доступа к телеграм-боту
+#     admin_ids: list[int]   # Список id администраторов бота
+#
+#
+# @dataclass
+# class Config:
+#     tg_bot: TgBot
+#     db: DatabaseConfig
+#
+#
+# config = Config(tg_bot=TgBot(token=BOT_TOKEN,
+#                              admin_ids=ADMIN_IDS),
+#                 db=DatabaseConfig(db_host=DB_HOST,
+#                                   db_user=DB_USER,
+#                                   db_password=DB_PASSWORD,
+#                                   database=DATABASE))
+
+
+
+
+
+
+
 
