80965575c951611337b323fef255880dd0b32d26
Paul <gafgafgarion05@gmail.com>
2023-02-03 09:23:14 +0300

    not 1 but 4 answers (or greetings dunno) which bot told me

--- tg_bot_main.py
+++ tg_bot_main.py
@@ -82,66 +82,68 @@ from typing import Union, List, Optional, Literal, Any
 # except:
 #     print(res.status_code)
 
-
-# import requests
-# import time
-#
-#
-# API_URL: str = 'https://api.telegram.org/bot'
-# BOT_TOKEN: str = '*'
-# TEXT: str = 'AMAZING!'
-# MAX_COUNTER: int = 100
-#
-# offset: int = -2
-# counter: int = 0
-# chat_id: int
-#
-#
-# while counter < MAX_COUNTER:
-#
-#     print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
-#
-#     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
-#
-#     if updates['result']:
-#         for result in updates['result']:
-#             offset = result['update_id']
-#             chat_id = result['message']['from']['id']
-#             requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
-#
-#     time.sleep(1)
-#     counter += 1
-
-
+import random
 import requests
 import time
 
 
 API_URL: str = 'https://api.telegram.org/bot'
-API_CATS_URL: str = 'https://aws.random.cat/meow'
 BOT_TOKEN: str = '*'
-ERROR_TEXT: str = 'Здесь должна была быть картинка с котиком :('
+# TEXT: str = 'Привет Паша :*'
+# TEXT: str = random.choice(['a', 'b', 'c', 'd'])
+MAX_COUNTER: int = 20
 
 offset: int = -2
 counter: int = 0
-cat_response: requests.Response
-cat_link: str
+chat_id: int
 
 
-while counter < 15:
-    print('attempt =', counter)
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            cat_response = requests.get(API_CATS_URL)
-            if cat_response.status_code == 200:
-                cat_link = cat_response.json()['file']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+            # requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+            requests.get(f"{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={random.choice(['Котик', 'Зая', ':*', 'Малыш'])}")
 
     time.sleep(1)
     counter += 1
+
+
+# import requests
+# import time
+#
+#
+# API_URL: str = 'https://api.telegram.org/bot'
+# API_CATS_URL: str = 'https://aws.random.cat/meow'
+# BOT_TOKEN: str = '*'
+# ERROR_TEXT: str = 'Здесь должна была быть картинка с котиком :('
+#
+# offset: int = -2
+# counter: int = 0
+# cat_response: requests.Response
+# cat_link: str
+#
+#
+# while counter < 15:
+#     print('attempt =', counter)
+#     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+#
+#     if updates['result']:
+#         for result in updates['result']:
+#             offset = result['update_id']
+#             chat_id = result['message']['from']['id']
+#             cat_response = requests.get(API_CATS_URL)
+#             if cat_response.status_code == 200:
+#                 cat_link = cat_response.json()['file']
+#                 requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+#             else:
+#                 requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+#
+#     time.sleep(1)
+#     counter += 1
