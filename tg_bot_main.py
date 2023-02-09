a68ba6a6142d0532dbde6ecc39de67e39e1c69bc
Paul <gafgafgarion05@gmail.com>
2023-02-09 10:26:17 +0300

    -

--- tg_bot_main.py
+++ tg_bot_main.py
@@ -246,38 +246,88 @@ from typing import Union, List, Optional, Literal, Any
 #     executor.start_polling(dp)
 
 
-
-from aiogram import Bot, Dispatcher, types
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import Text
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.utils import executor
 import random
+import telebot
+# from telebot import types
+import logging
 
-bot = Bot(token=TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
+API_TOKEN = TOKEN
+
+bot = telebot.TeleBot(API_TOKEN)
+# telebot.logger.setLevel(logging.INFO)
+telebot.setLevel(logging.INFO)
+
+storage = dict()
 
 
-class Form(StatesGroup):
-    random_number = State()
-    number = State()
+def init_storage(user_id):
+    storage[user_id] = dict(attempt=None, random_digit=None)
 
 
-@dp.message_handler(commands=['help'])
-async def help(message: types.Message):
-    await message.reply('Hi! I am the Telegram bot. I can play Guess the number game. /start to start the game.')
+def set_data_storage(user_id, key, value):
+    storage[user_id][key] = value
 
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await Form.random_number.set()
-    await bot.send_message(message.chat.id, 'I am thinking of a number from 1 to 100. Try to guess it. (/cancel to stop the game)')
 
-@dp.message_handler(state='*', commands='cancel')
-@dp.message_handler(Text(equals='stop', ignore_case=True), state='*')
-async def cancel_handler(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    if current_state is None:
+def get_data_storage(user_id):
+    return storage[user_id]
+
+
+@bot.message_handler(func=lambda message: message.text.lower() == "привет")
+def command_text_hi(m):
+    bot.send_message(m.chat.id, "Ну привет)")
+
+
+@bot.message_handler(func=lambda message: message.text.lower() == "как дела")
+def command_text_dela(m):
+    bot.send_message(m.chat.id, "хорошо")
+
+
+@bot.message_handler(func=lambda message: message.text.lower() == "игра")
+def digitgames(message):
+    init_storage(message.chat.id)  ### Инициализирую хранилище
+
+    attempt = 5
+    set_data_storage(message.chat.id, "attempt", attempt)
+
+    bot.send_message(message.chat.id, f'Игра "угадай число"!\nКоличество попыток: {attempt}')
+
+    random_digit = random.randint(1, 10)
+    print(random_digit)
+
+    set_data_storage(message.chat.id, "random_digit", random_digit)
+    print(get_data_storage(message.chat.id))
+
+    bot.send_message(message.chat.id, 'Готово! Загадано число от 1 до 10!')
+    bot.send_message(message.chat.id, 'Введите число')
+    bot.register_next_step_handler(message, process_digit_step)
+
+
+def process_digit_step(message):
+    user_digit = message.text
+
+    if not user_digit.isdigit():
+        msg = bot.reply_to(message, 'Вы ввели не цифры, введите пожалуйста цифры')
+        bot.register_next_step_handler(msg, process_digit_step)
         return
-    await state.finish()
+
+    attempt = get_data_storage(message.chat.id)["attempt"]
+    random_digit = get_data_storage(message.chat.id)["random_digit"]
+
+    if int(user_digit) == random_digit:
+        bot.send_message(message.chat.id, f'Ура! Ты угадал число! Это была цифра: {random_digit}')
+        init_storage(message.chat.id)  ### Очищает значения из хранилище
+        return
+    elif attempt > 1:
+        attempt -= 1
+        set_data_storage(message.chat.id, "attempt", attempt)
+        bot.send_message(message.chat.id, f'Неверно, осталось попыток: {attempt}')
+        bot.register_next_step_handler(message, process_digit_step)
+    else:
+        bot.send_message(message.chat.id, 'Вы проиграли!')
+        init_storage(message.chat.id)  ### Очищает значения из хранилище
+        return
+
+
+if __name__ == '__main__':
+    bot.skip_pending = True
+    bot.polling()
