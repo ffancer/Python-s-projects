5f27403c657b9d1b2ba0ff1f6434c17e013d4ad3
Paul <gafgafgarion05@gmail.com>
2023-02-10 17:12:07 +0300

    -

--- tg_bot_main.py
+++ tg_bot_main.py
@@ -246,88 +246,35 @@ from typing import Union, List, Optional, Literal, Any
 #     executor.start_polling(dp)
 
 
-import random
-import telebot
 # from telebot import types
-import logging
 
-API_TOKEN = TOKEN
-
-bot = telebot.TeleBot(API_TOKEN)
-# telebot.logger.setLevel(logging.INFO)
-telebot.setLevel(logging.INFO)
-
-storage = dict()
+import telebot
+import random
+import config
 
 
-def init_storage(user_id):
-    storage[user_id] = dict(attempt=None, random_digit=None)
+@bot.message_handler(commands=['start'])
+def start(message: types.Message):
+    rnumber = random.randint(0, 100)
+    bot.send_message(message.chat.id, 'Отгадайте число от 0 до 100, у вас 5 попыток')
+    bot.register_next_step_handler(message, get_number, 1, rnumber)
 
 
-def set_data_storage(user_id, key, value):
-    storage[user_id][key] = value
+def get_number(message: types.Message, try_num: int, right_num: int):
+    if message.text.isdigit():
+        if right_num == int(message.text):
+            bot.send_message(message.chat.id, 'Вы отгадали число!')
+        else:
+            if try_num >= 4:
+                bot.send_message(message.chat.id, 'Мимо! Попытки кончились!')
+                return
 
+            if right_num < int(message.text):
+                bot.send_message(message.chat.id, 'Загаданное число меньше введенного.')
+            else:
+                bot.send_message(message.chat.id, 'Загаданное число больше введенного.')
+            bot.register_next_step_handler(message, get_number, try_num + 1, right_num)
 
-def get_data_storage(user_id):
-    return storage[user_id]
-
-
-@bot.message_handler(func=lambda message: message.text.lower() == "привет")
-def command_text_hi(m):
-    bot.send_message(m.chat.id, "Ну привет)")
-
-
-@bot.message_handler(func=lambda message: message.text.lower() == "как дела")
-def command_text_dela(m):
-    bot.send_message(m.chat.id, "хорошо")
-
-
-@bot.message_handler(func=lambda message: message.text.lower() == "игра")
-def digitgames(message):
-    init_storage(message.chat.id)  ### Инициализирую хранилище
-
-    attempt = 5
-    set_data_storage(message.chat.id, "attempt", attempt)
-
-    bot.send_message(message.chat.id, f'Игра "угадай число"!\nКоличество попыток: {attempt}')
-
-    random_digit = random.randint(1, 10)
-    print(random_digit)
-
-    set_data_storage(message.chat.id, "random_digit", random_digit)
-    print(get_data_storage(message.chat.id))
-
-    bot.send_message(message.chat.id, 'Готово! Загадано число от 1 до 10!')
-    bot.send_message(message.chat.id, 'Введите число')
-    bot.register_next_step_handler(message, process_digit_step)
-
-
-def process_digit_step(message):
-    user_digit = message.text
-
-    if not user_digit.isdigit():
-        msg = bot.reply_to(message, 'Вы ввели не цифры, введите пожалуйста цифры')
-        bot.register_next_step_handler(msg, process_digit_step)
-        return
-
-    attempt = get_data_storage(message.chat.id)["attempt"]
-    random_digit = get_data_storage(message.chat.id)["random_digit"]
-
-    if int(user_digit) == random_digit:
-        bot.send_message(message.chat.id, f'Ура! Ты угадал число! Это была цифра: {random_digit}')
-        init_storage(message.chat.id)  ### Очищает значения из хранилище
-        return
-    elif attempt > 1:
-        attempt -= 1
-        set_data_storage(message.chat.id, "attempt", attempt)
-        bot.send_message(message.chat.id, f'Неверно, осталось попыток: {attempt}')
-        bot.register_next_step_handler(message, process_digit_step)
     else:
-        bot.send_message(message.chat.id, 'Вы проиграли!')
-        init_storage(message.chat.id)  ### Очищает значения из хранилище
-        return
-
-
-if __name__ == '__main__':
-    bot.skip_pending = True
-    bot.polling()
+        bot.send_message(message.chat.id, 'Вы ввели не число. Попробуйте еще раз.')
+        bot.register_next_step_handler(message, get_number, try_num, right_num)
