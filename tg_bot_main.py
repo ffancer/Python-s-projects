865872234abde285ceef91031347f0f795f21e9c
Paul <gafgafgarion05@gmail.com>
2023-02-20 12:06:24 +0300

    -

--- tg_bot_main.py
+++ tg_bot_main.py
@@ -333,10 +333,9 @@ async def get_weather(message: types.Message):
         sunset = datetime.datetime.fromtimestamp(data['sys']['sunset'])
         day_length = sunset - sunrise
 
-        await message.reply(f'       {time_now}       \nТемпература {temp} °C {wd}\nОщущается как {feels_like} °C\n' \
+        await message.reply(f'Время и число: {time_now}\nТемпература {temp} °C {wd}\nОщущается как {feels_like} °C\n' \
                             f'Давление {pressure} мм.рт.ст\nВетер {wind} м\сек\nРассвет {sunrise}\nЗакат {sunset}\n' \
-                            f'Продолжительность дня: {day_length}', case_insensitive=True)
-
+                            f'Продолжительность дня: {day_length}')
     except KeyError:
         await message.reply('\U00002620 Неверное название города \U00002620\n Введите город: ')
 
