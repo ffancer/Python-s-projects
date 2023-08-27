c019fabc26d9c3aeef602f11aa3f909d2d918a2c
Paul <gafgafgarion05@gmail.com>
2023-08-27 09:38:57 +0300

    make board with num-s

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,2 +1,13 @@
-a = 0
-print(a)
+board = ['1', '2', '3',
+         '4', '5', '6',
+         '7', '8', '9'
+         ]
+
+
+def show_board():
+    print(f'{board[0]} | {board[1]} | {board[2]}')
+    print(f'{board[3]} | {board[4]} | {board[5]}')
+    print(f'{board[6]} | {board[7]} | {board[8]}')
+
+
+show_board()
