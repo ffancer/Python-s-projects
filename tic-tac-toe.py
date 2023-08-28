f64b0ce173b4f5daef0323005d4057f693c238b8
Paul <gafgafgarion05@gmail.com>
2023-08-28 10:14:17 +0300

    try to use random for interesting game(opponent)

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,3 +1,5 @@
+from random import randint
+from time import sleep
 # board = ['1', '2', '3',
 #          '4', '5', '6',
 #          '7', '8', '9'
@@ -13,14 +15,29 @@ def show_board():
 
 show_board()
 
+
 flag = True
 while flag:
-    num = input('num: ')
-    if num in board:
-        board = board.replace(num, 'X')
+    computer_num = str(randint(1, 9))
+    if computer_num in board:
+        board = board.replace(computer_num, 'X')
     show_board()
+    sleep(1)
     if board[0] == board[1] == board[2] or board[0] == board[4] == board[8] or \
             board[3] == board[4] == board[5] or board[6] == board[7] == board[8] or \
             board[6] == board[4] == board[2]:
         print('You win')
         flag = False
+
+# победы
+# flag = True
+# while flag:
+#     num = input('num: ')
+#     if num in board:
+#         board = board.replace(num, 'X')
+#     show_board()
+#     if board[0] == board[1] == board[2] or board[0] == board[4] == board[8] or \
+#             board[3] == board[4] == board[5] or board[6] == board[7] == board[8] or \
+#             board[6] == board[4] == board[2]:
+#         print('You win')
+#         flag = False
