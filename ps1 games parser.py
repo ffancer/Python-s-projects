20675e352464b16985cbb1d4e4e11f10265e27b6
Paul <gafgafgarion05@gmail.com>
2023-12-28 11:48:35 +0300

    all games were saved in json file

--- ps1 games parser.py
+++ ps1 games parser.py
@@ -7,51 +7,44 @@ import pandas as pd
 import json
 
 
-json_list = []
-# выбрал на сайте игры из секции пс1
-# url = 'https://www.romhacking.net/?page=hacks&category=&platform=17&game=&perpage=20&order=&title=&dir=&hacksearch=Go'
-#
-# headers = {
-#     'Accept': '*/*',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-# }
-#
-# req = requests.get(url, headers)
-# src = req.text
-#
-# # сохраняем файл на комп, что бы не получить бан
-# with open('ps1games.html', 'w', encoding="utf-8") as file:
-#     file.write(src)
+def collect_data():
+    json_list = []
 
-with open('ps1games.html', encoding="utf-8") as file:
-    src = file.read()
+    for i in range(1, 11):
+        url = f'https://www.romhacking.net/?page=hacks&platform=17&perpage=20&startpage={i}'
 
-soup = BeautifulSoup(src, 'lxml')
-all_hrefs = soup.find_all(class_='even') + soup.find_all(class_='odd')
-for item in all_hrefs:
-    # patch_name = item.find('a').text
-    # print(patch_name)
-    # print(item.find('td class_3'))
-    # print(item.find(class_='col_2 Released By').text)
-    original_game_name = item.find(class_='col_3 Original Game').text
-    game_genre = item.find(class_='col_4 Genre').text
-    category = item.find(class_='col_7 Category').text
-    date = item.find(class_='col_9 Date').text
-    # print(f'game - {original_game_name} || genre: {game_genre} || category - {category} || date: {date}')
-    json_list.append(
-        {
-            'Game': original_game_name,
-            'Genre': game_genre,
-            'Category': category,
-            'Date': date
+        headers = {
+            'Accept': '*/*',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
         }
-    )
+
+        req = requests.get(url, headers)
+        src = req.text
+        soup = BeautifulSoup(src, 'lxml')
+        all_hrefs = soup.find_all(class_='even') + soup.find_all(class_='odd')
+
+        for item in all_hrefs:
+            original_game_name = item.find(class_='col_3 Original Game').text
+            game_genre = item.find(class_='col_4 Genre').text
+            category = item.find(class_='col_7 Category').text
+            date = item.find(class_='col_9 Date').text
+            json_list.append(
+                {
+                    'Game': original_game_name,
+                    'Genre': game_genre,
+                    'Category': category,
+                    'Date': date
+                }
+            )
+
+
+    return json_list
 
 
 def save_json_file():
     with open('top ps1 games.json', 'a', encoding='utf-8') as file:
-        json.dump(json_list, file, indent=4, ensure_ascii=False)
+        json.dump(collect_data(), file, indent=4, ensure_ascii=False)
 
 
 save_json_file()
-# первая страница https://www.romhacking.net/?page=hacks&platform=17&perpage=20&startpage=1 потом только цыфра меняется
+
