168c35dc3a7c50024688e6cdf5f2fa0d51f2245e
Paul <gafgafgarion05@gmail.com>
2023-12-28 11:50:22 +0300

    games were saved in Excel file

--- ps1 games parser.py
+++ ps1 games parser.py
@@ -7,44 +7,50 @@ import pandas as pd
 import json
 
 
-def collect_data():
-    json_list = []
-
-    for i in range(1, 11):
-        url = f'https://www.romhacking.net/?page=hacks&platform=17&perpage=20&startpage={i}'
-
-        headers = {
-            'Accept': '*/*',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-        }
-
-        req = requests.get(url, headers)
-        src = req.text
-        soup = BeautifulSoup(src, 'lxml')
-        all_hrefs = soup.find_all(class_='even') + soup.find_all(class_='odd')
-
-        for item in all_hrefs:
-            original_game_name = item.find(class_='col_3 Original Game').text
-            game_genre = item.find(class_='col_4 Genre').text
-            category = item.find(class_='col_7 Category').text
-            date = item.find(class_='col_9 Date').text
-            json_list.append(
-                {
-                    'Game': original_game_name,
-                    'Genre': game_genre,
-                    'Category': category,
-                    'Date': date
-                }
-            )
-
-
-    return json_list
-
-
-def save_json_file():
-    with open('top ps1 games.json', 'a', encoding='utf-8') as file:
-        json.dump(collect_data(), file, indent=4, ensure_ascii=False)
+# def collect_data():
+#     json_list = []
+#
+#     for i in range(1, 11):
+#         url = f'https://www.romhacking.net/?page=hacks&platform=17&perpage=20&startpage={i}'
+#
+#         headers = {
+#             'Accept': '*/*',
+#             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+#         }
+#
+#         req = requests.get(url, headers)
+#         src = req.text
+#         soup = BeautifulSoup(src, 'lxml')
+#         all_hrefs = soup.find_all(class_='even') + soup.find_all(class_='odd')
+#
+#         for item in all_hrefs:
+#             original_game_name = item.find(class_='col_3 Original Game').text
+#             game_genre = item.find(class_='col_4 Genre').text
+#             category = item.find(class_='col_7 Category').text
+#             date = item.find(class_='col_9 Date').text
+#             json_list.append(
+#                 {
+#                     'Game': original_game_name,
+#                     'Genre': game_genre,
+#                     'Category': category,
+#                     'Date': date
+#                 }
+#             )
+#
+#
+#     return json_list
 
 
-save_json_file()
+# def save_json_file():
+#     with open('top ps1 games.json', 'a', encoding='utf-8') as file:
+#         json.dump(collect_data(), file, indent=4, ensure_ascii=False)
 
+
+# save_json_file()
+def from_json_to_excel():
+    with open('top ps1 games.json', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+
+    df = pd.DataFrame(data)
+    return df.to_excel('top ps1 games.xlsx')
+from_json_to_excel()
